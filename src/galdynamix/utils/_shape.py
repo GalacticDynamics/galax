8152c537cc57d35302197e4778cfd45cdf3c5353
nstarman <nstarman@users.noreply.github.com>
2023-12-08 22:24:33 -0500

    Add batch, array shape utils
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galdynamix/utils/_shape.py
+++ src/galdynamix/utils/_shape.py
@@ -122,3 +122,74 @@ def batched_shape(
     shape: tuple[int, ...] = xp.shape(arr)
     ndim = len(shape)
     return shape[: ndim - expect_ndim], shape[ndim - expect_ndim :]
+
+
+def expand_batch_dims(arr: ArrayAnyShape, /, ndim: int) -> ArrayAnyShape:
+    """Expand the batch dimensions of an array.
+
+    Parameters
+    ----------
+    arr : array-like
+        The array to expand the batch dimensions of.
+    ndim : int
+        The number of batch dimensions to expand.
+
+    Returns
+    -------
+    arr : array-like
+        The array with expanded batch dimensions.
+
+    Examples
+    --------
+    >>> import jax.numpy as jnp
+    >>> from galdynamix.utils._shape import expand_batch_dims
+
+    >>> expand_batch_dims(jnp.array(0), ndim=0).shape
+    ()
+
+    >>> expand_batch_dims(jnp.array([0]), ndim=0).shape
+    (1,)
+
+    >>> expand_batch_dims(jnp.array(0), ndim=1).shape
+    (1,)
+
+    >>> expand_batch_dims(jnp.array([0, 1]), ndim=1).shape
+    (1, 2)
+    """
+    return xp.expand_dims(arr, axis=tuple(0 for _ in range(ndim)))
+
+
+def expand_arr_dims(arr: ArrayAnyShape, /, ndim: int) -> ArrayAnyShape:
+    """Expand the array dimensions of an array.
+
+    Parameters
+    ----------
+    arr : array-like
+        The array to expand the array dimensions of.
+    ndim : int
+        The number of array dimensions to expand.
+
+    Returns
+    -------
+    arr : array-like
+        The array with expanded array dimensions.
+
+    Examples
+    --------
+    >>> import jax.numpy as jnp
+    >>> from galdynamix.utils._shape import expand_arr_dims
+
+    >>> expand_arr_dims(jnp.array(0), ndim=0).shape
+    ()
+
+    >>> expand_arr_dims(jnp.array([0]), ndim=0).shape
+    (1,)
+
+    >>> expand_arr_dims(jnp.array(0), ndim=1).shape
+    (1,)
+
+    >>> expand_arr_dims(jnp.array([0, 0]), ndim=1).shape
+    (2, 1)
+    """
+    nbatch = len(arr.shape)
+    return xp.expand_dims(arr, axis=tuple(nbatch + i for i in range(ndim)))

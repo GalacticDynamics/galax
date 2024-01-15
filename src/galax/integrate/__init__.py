49736d7d432bde56c627650a209fc7a86d2dab43
nstarman <nstarman@users.noreply.github.com>
2024-01-14 20:51:32 -0500

    Add parse_time_specification
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/integrate/__init__.py
+++ src/galax/integrate/__init__.py
@@ -1,10 +1,12 @@
 """galax: Galactic Dynamix in Jax."""
 
 
-from galax.integrate import _base, _builtin
+from galax.integrate import _base, _builtin, _timespec
 from galax.integrate._base import *
 from galax.integrate._builtin import *
+from galax.integrate._timespec import *
 
 __all__: list[str] = []
 __all__ += _base.__all__
 __all__ += _builtin.__all__
+__all__ += _timespec.__all__
---
+++ src/galax/integrate/_timespec.py
@@ -0,0 +1,247 @@
+"""Time specification helper function.
+
+Helper function for turning different ways of specifying the integration times
+into an array of times.
+"""
+
+__all__ = ["parse_time_specification"]
+
+from typing import TypeVar, cast, overload
+
+import jax
+import jax.numpy as jnp
+from astropy.units import Quantity
+from jaxtyping import Array, Float
+from plum import dispatch as dispatch_
+
+from galax.typing import FloatScalar
+from galax.units import UnitSystem
+
+T = TypeVar("T")
+
+
+def dispatch(func: T) -> T:  # TODO: fix mypy complaint about untyped decorator
+    return cast("T", dispatch_(func))
+
+
+# ===========================================================================
+
+
+@dispatch
+def parse_time_specification_dispatch(
+    units: UnitSystem,
+    t: None,
+    t1: None,
+    t2: None,
+    n_steps: None,
+    dt: None,
+) -> Float[Array, "N"]:
+    msg = "must specify some combination of t, t1, t2, n_steps, dt"
+    raise ValueError(msg)
+
+
+@dispatch  # type: ignore[no-redef]
+def parse_time_specification_dispatch(  # noqa: F811
+    units: UnitSystem,
+    t: Float[Array, "N"] | jax.Array,
+    t1: None,
+    t2: None,
+    n_steps: None,
+    dt: None,
+) -> Float[Array, "N"]:
+    return jnp.asarray(t, dtype=float)
+
+
+@dispatch  # type: ignore[no-redef]
+def parse_time_specification_dispatch(  # noqa: F811
+    units: UnitSystem,
+    t: Quantity,
+    t1: None,
+    t2: None,
+    n_steps: None,
+    dt: None,
+) -> Float[Array, "N"]:
+    return jnp.asarray(t.decompose(units).value, dtype=float)
+
+
+@dispatch  # type: ignore[no-redef]
+def parse_time_specification_dispatch(  # noqa: F811
+    units: UnitSystem,
+    t: None,
+    t1: float | Quantity | FloatScalar,
+    t2: float | Quantity | FloatScalar,
+    n_steps: int,
+    dt: None,
+) -> Float[Array, "N"]:
+    if isinstance(t1, Quantity):
+        t1 = t1.decompose(units).value
+    if isinstance(t2, Quantity):
+        t2 = t2.decompose(units).value
+
+    return jnp.linspace(t1, t2, n_steps, dtype=float)
+
+
+@dispatch  # type: ignore[no-redef]
+def parse_time_specification_dispatch(  # noqa: F811
+    units: UnitSystem,
+    t: None,
+    t1: float | Quantity | FloatScalar,
+    t2: None,
+    n_steps: int,
+    dt: float | Quantity | FloatScalar,
+) -> Float[Array, "N"]:
+    if isinstance(t1, Quantity):
+        t1 = t1.decompose(units).value
+    if isinstance(dt, Quantity):
+        dt = dt.decompose(units).value
+
+    return jnp.arange(t1, t1 + dt * n_steps, dt, dtype=float)
+
+
+@dispatch  # type: ignore[no-redef]
+def parse_time_specification_dispatch(  # noqa: F811
+    units: UnitSystem,
+    t: None,
+    t1: float | Quantity | FloatScalar,
+    t2: float | Quantity | FloatScalar,
+    n_steps: None,
+    dt: float | Quantity | FloatScalar,
+) -> Float[Array, "N"]:
+    if isinstance(t1, Quantity):
+        t1 = t1.decompose(units).value
+    if isinstance(t2, Quantity):
+        t2 = t2.decompose(units).value
+    if isinstance(dt, Quantity):
+        dt = dt.decompose(units).value
+
+    return jnp.arange(t1, t2, dt, dtype=float)
+
+
+@dispatch  # type: ignore[no-redef]
+def parse_time_specification_dispatch(  # noqa: F811
+    units: UnitSystem,
+    t: None,
+    t1: float | Quantity | FloatScalar,
+    t2: None,
+    n_steps: None,
+    dt: Quantity | Float[Array, "N"],
+) -> Float[Array, "N"]:
+    if isinstance(t1, Quantity):
+        t1 = t1.decompose(units).value
+    if isinstance(dt, Quantity):
+        dt = dt.decompose(units).value
+
+    return t1 + jnp.cumsum(dt, dtype=float)
+
+
+# ===========================================================================
+
+
+@overload
+def parse_time_specification(
+    units: UnitSystem,
+    *,
+    t: Quantity | Float[Array, "N"],
+    t1: None,
+    t2: None,
+    n_steps: None,
+    dt: None,
+) -> Float[Array, "N"]:
+    ...
+
+
+@overload
+def parse_time_specification(
+    units: UnitSystem,
+    *,
+    t: None,
+    t1: float | Quantity | FloatScalar,
+    t2: float | Quantity | FloatScalar,
+    n_steps: int,
+    dt: None,
+) -> Float[Array, "N"]:
+    ...
+
+
+@overload
+def parse_time_specification(
+    units: UnitSystem,
+    *,
+    t: None,
+    t1: float | Quantity | FloatScalar,
+    t2: None,
+    n_steps: int,
+    dt: float | Quantity | FloatScalar,
+) -> Float[Array, "N"]:
+    ...
+
+
+@overload
+def parse_time_specification(
+    units: UnitSystem,
+    *,
+    t: None,
+    t1: float | Quantity | FloatScalar,
+    t2: float | Quantity | FloatScalar,
+    n_steps: None,
+    dt: float | Quantity | FloatScalar,
+) -> Float[Array, "N"]:
+    ...
+
+
+@overload
+def parse_time_specification(
+    units: UnitSystem,
+    *,
+    t: None,
+    t1: float | Quantity | FloatScalar,
+    t2: None,
+    n_steps: None,
+    dt: Quantity | Float[Array, "N"],
+) -> Float[Array, "N"]:
+    ...
+
+
+def parse_time_specification(
+    units: UnitSystem,
+    *,
+    t: Quantity | Float[Array, "N"] = None,
+    t1: float | Quantity | FloatScalar | None = None,
+    t2: float | Quantity | FloatScalar | None = None,
+    n_steps: int | None = None,
+    dt: float | Quantity | FloatScalar | Float[Array, "N"] | None = None,
+) -> Float[Array, "N"]:
+    """Return an array of times given a few combinations of kwargs.
+
+    Parameters
+    ----------
+    units : UnitSystem
+        The unit system to use.
+
+    t : Array[N], optional keyword-only
+        An array of times.
+    t1, t2 : numeric scalar, optional keyword-only
+        An initial/final time.
+    n_steps : int, optional keyword-only
+        The number of steps between an initial and final time.
+    dt : numeric scalar, optional keyword-only
+        A fixed timestep.
+
+    Notes
+    -----
+    The following combinations of kwargs are allowed: The groups are separated
+    by whether they have a pre-fixed length (suitable for jit) or not.
+
+    - (t: array)
+        The full array of times (dts = t[1:] - t[:-1]).
+    - (t1: float, t2: float, n_steps: int)
+        Number of steps between an initial time, and a final time.
+    - (t1: float, n_steps: float, dt: float)
+        An initial time, a number of steps, and a fixed timestep.
+
+    - (t1: float, t2: float, dt: int)
+        An initial time, a final time, and a fixed timestep.
+    - (t1: float, dt: array)
+        An initial time and an array of timesteps dt.
+    """
+    return parse_time_specification_dispatch(units, t, t1, t2, n_steps, dt)

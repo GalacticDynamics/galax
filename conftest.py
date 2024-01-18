dcd0249fc7a0e0a1e051ba0938f811c06e70fa09
nstarman <nstarman@users.noreply.github.com>
2024-01-18 15:15:57 -0500

    doctest normalize whitespace
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- conftest.py
+++ conftest.py
@@ -2,14 +2,14 @@
 
 from __future__ import annotations
 
-from doctest import ELLIPSIS
+from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
 
 from sybil import Sybil
 from sybil.parsers.rest import DocTestParser, PythonCodeBlockParser, SkipParser
 
 pytest_collect_file = Sybil(
     parsers=[
-        DocTestParser(optionflags=ELLIPSIS),
+        DocTestParser(optionflags=ELLIPSIS | NORMALIZE_WHITESPACE),
         PythonCodeBlockParser(),
         SkipParser(),
     ],

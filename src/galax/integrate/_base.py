3e15be4bdb3b2f123a3251f99713f26b27c52dda
nstarman <nstarman@users.noreply.github.com>
2024-01-22 14:56:40 -0500

    Provide Protocol for Integrators
    Allows any function meeting the protocol to be used as an Integrator instance.
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>
    
    rm extraneous type hint
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/integrate/_base.py
+++ src/galax/integrate/_base.py
@@ -1,4 +1,4 @@
-__all__ = ["AbstractIntegrator"]
+__all__ = ["Integrator", "AbstractIntegrator"]
 
 import abc
 from typing import Any, Protocol, runtime_checkable
@@ -7,6 +7,7 @@ import equinox as eqx
 from jaxtyping import Array, Float
 
 from galax.typing import FloatScalar, Vec6
+from galax.utils.dataclasses import _DataclassInstance
 
 
 @runtime_checkable
@@ -31,31 +32,73 @@ class FCallable(Protocol):
         ...
 
 
-class AbstractIntegrator(eqx.Module, strict=True):  # type: ignore[call-arg, misc]
-    """Integrator Class.
+@runtime_checkable
+class Integrator(_DataclassInstance, Protocol):
+    """:class:`typing.Protocol` for integrators.
 
     The integrators are classes that are used to integrate the equations of
     motion.
     They must not be stateful since they are used in a functional way.
     """
 
+    def __call__(
+        self, F: FCallable, qp0: Vec6, /, ts: Float[Array, "T"] | None
+    ) -> Float[Array, "R 7"]:
+        """Integrate.
+
+        Parameters
+        ----------
+        F : FCallable, positional-only
+            The function to integrate.
+            (t, qp, args) -> (v, a).
+        qp0 : Array[float, (6,)], positional-only
+            Initial conditions ``[q, p]``.
+
+        ts : Array[float, (T,)] | None
+            Times to return the computation.
+            It's necessary to at least provide the initial and final times.
+
+        Returns
+        -------
+        Array[float, (R, 7)]
+            The solution of the integrator [q, p, t], where q, p are the
+            generalized 3-coordinates.
+        """
+        ...
+
+
+class AbstractIntegrator(eqx.Module, strict=True):  # type: ignore[call-arg, misc]
+    """Abstract base class for integrators.
+
+    This class is the base for the hierarchy of concrete integrator classes
+    provided in this package. It is not necessary, but it is recommended, to
+    inherit from this class to implement an integrator. The Protocol
+    :class:`Integrator` must be implemented.
+
+    The integrators are classes that are used to integrate the equations of
+    motion.  They must not be stateful since they are used in a functional way.
+    """
+
     @abc.abstractmethod
-    def run(
+    def __call__(
         self,
         F: FCallable,
         qp0: Vec6,
+        /,
         ts: Float[Array, "T"],
     ) -> Float[Array, "T 7"]:
         """Run the integrator.
 
         Parameters
         ----------
-        F : FCallable
+        F : FCallable, positional-only
             The function to integrate.
-        qp0 : Array[float, (6,)]
+        qp0 : Array[float, (6,)], positional-only
             Initial conditions ``[q, p]``.
+
         ts : Array[float, (T,)] | None
             Times to return the computation.
+            It's necessary to at least provide the initial and final times.
 
         Returns
         -------

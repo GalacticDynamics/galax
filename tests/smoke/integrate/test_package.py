cfb70f23ed3fa090e50b048790c17a28753fb885
nstarman <nstarman@users.noreply.github.com>
2024-01-22 22:04:41 -0500

    API smoke test
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- tests/smoke/integrate/test_package.py
+++ tests/smoke/integrate/test_package.py
@@ -6,5 +6,7 @@ from galax import integrate
 def test_all():
     """Test the API."""
     assert set(integrate.__all__) == set(
-        integrate._base.__all__ + integrate._builtin.__all__
+        integrate._base.__all__
+        + integrate._builtin.__all__
+        + integrate._timespec.__all__
     )

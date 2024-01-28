6fcc6a8a44fe929c3d7d7bcf8b9c1bdbabf01ab3
nstarman <nstarman@users.noreply.github.com>
2024-01-28 16:54:20 -0500

    refactor(jax-utils): remove partial_vectorize

--- tests/unit/utils/test_jax.py
+++ tests/unit/utils/test_jax.py
@@ -1,21 +1,7 @@
 import jax.numpy as jnp
 from jaxtyping import Array, Float
 
-from galax.utils import partial_vectorize, vectorize_method
-
-
-def test_partial_vectorize():
-    """Test the partial_vectorize function."""
-
-    def func(x: Float[Array, "batch N"]) -> Float[Array, "batch"]:
-        return jnp.sum(x)
-
-    vectorize_func = partial_vectorize(signature="(3)->()")(func)
-    assert vectorize_func(jnp.array([1, 2, 3])) == 6
-
-    # The real test is comparing this to the output of `jax.vectorize`.
-    x = jnp.array([1, 2, 3])
-    assert vectorize_func(x) == jnp.vectorize(func, signature="(3)->()")(x)
+from galax.utils import vectorize_method
 
 
 def test_vectorize_method():

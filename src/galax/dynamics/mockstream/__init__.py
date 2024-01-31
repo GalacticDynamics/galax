8e66882e053271ff94c569ab92cfb19848c25c7c
nstarman <nstarman@users.noreply.github.com>
2024-01-30 23:08:19 -0500

    feat(stream): make MockStream class public
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/dynamics/mockstream/__init__.py
+++ src/galax/dynamics/mockstream/__init__.py
@@ -1,9 +1,11 @@
 """galax: Galactic Dynamix in Jax."""
 
-from . import _df, _mockstream_generator
+from . import _core, _df, _mockstream_generator
+from ._core import *
 from ._df import *
 from ._mockstream_generator import *
 
 __all__: list[str] = []
 __all__ += _df.__all__
+__all__ += _core.__all__
 __all__ += _mockstream_generator.__all__

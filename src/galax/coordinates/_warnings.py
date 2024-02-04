54fba7814cc263c772c6df08f1c9d78ce9b79ce9
nstarman <nstarman@users.noreply.github.com>
2024-02-03 23:03:57 -0500

    feat: ignoredtime warning
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

---
+++ src/galax/coordinates/_warnings.py
@@ -0,0 +1,9 @@
+"""galax: Galactic Dynamix in Jax."""
+
+__all__: list[str] = []
+
+
+# TODO: make public
+# TODO: customize the automatic message
+class IgnoredTimeWarning(UserWarning):
+    """Warning for ignored time."""

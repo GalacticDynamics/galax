fix: test units
Signed-off-by: nstarman <nstarman@users.noreply.github.com>

tests/unit/coordinates/test_pspt.py
@@ -70,8 +70,12 @@ class AbstractPhaseSpaceTimePosition_Test(AbstractPhaseSpacePositionBase_Test[T]
         wt = w.wt(units=galactic)
         assert wt.shape == w.full_shape
         assert jnp.array_equal(wt[..., 0], w.t)
-        assert jnp.array_equal(wt[..., 1:4], convert(w.q, Quantity).value)
-        assert jnp.array_equal(wt[..., 4:7], convert(w.p, Quantity).value)
+        assert jnp.array_equal(
+            wt[..., 1:4], convert(w.q, Quantity).decompose(galactic).value
+        )
+        assert jnp.array_equal(
+            wt[..., 4:7], convert(w.p, Quantity).decompose(galactic).value
+        )
 
     # ===============================================================
 

tests/unit/dynamics/test_orbit.py
@@ -73,5 +73,9 @@ class TestOrbit(AbstractPhaseSpaceTimePosition_Test[Orbit]):
         wt = w.wt(units=galactic)
         assert wt.shape == w.full_shape
         assert jnp.array_equal(wt[(*(0,) * (w.ndim - 1), slice(None), 0)], w.t)
-        assert jnp.array_equal(wt[..., 1:4], convert(w.q, Quantity).value)
-        assert jnp.array_equal(wt[..., 4:7], convert(w.p, Quantity).value)
+        assert jnp.array_equal(
+            wt[..., 1:4], convert(w.q, Quantity).decompose(galactic).value
+        )
+        assert jnp.array_equal(
+            wt[..., 4:7], convert(w.p, Quantity).decompose(galactic).value
+        )
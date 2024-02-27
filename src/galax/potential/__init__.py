5db150413912253f79740748bec8f394a62d8526
nstarman <nstarman@users.noreply.github.com>
2024-02-27 02:12:07 -0500

    fix: use typecheck import hook
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/potential/__init__.py
+++ src/galax/potential/__init__.py
@@ -5,9 +5,9 @@ from lazy_loader import attach_stub
 
 from galax.setup_package import RUNTIME_TYPECHECKER
 
-with install_import_hook("galax.potential", RUNTIME_TYPECHECKER):
-    __getattr__, __dir__, __all__ = attach_stub(__name__, __file__)
+__getattr__, __dir__, __all__ = attach_stub(__name__, __file__)
 
+install_import_hook("galax.potential", RUNTIME_TYPECHECKER)
 
 # Cleanup
 del install_import_hook, RUNTIME_TYPECHECKER

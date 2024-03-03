12cf0746e57e8efb2f974a6d63ac591a74a6dd0e
nstarman <nstarman@users.noreply.github.com>
2024-03-02 19:03:09 -0500

    feat: apply op to Quantity
    Interpreting as a CartesianVector
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/coordinates/operators/base.py
+++ src/galax/coordinates/operators/base.py
@@ -8,9 +8,10 @@ from dataclasses import replace
 from typing import TYPE_CHECKING, Any, TypeVar
 
 import equinox as eqx
+from jaxtyping import Shaped
 from plum import dispatch
 
-from coordinax import Abstract3DVector, AbstractVector, FourVector
+from coordinax import Abstract3DVector, AbstractVector, Cartesian3DVector, FourVector
 from jax_quantity import Quantity
 
 from galax.coordinates._psp.base import AbstractPhaseSpacePositionBase
@@ -76,15 +77,130 @@ class AbstractOperator(eqx.Module):  # type: ignore[misc]
         t: Quantity["time"],  # noqa: ARG002
         /,
     ) -> tuple[Abstract3DVector, Quantity["time"]]:
-        """Apply the operator to the coordinates."""
+        """Apply the operator to the coordinates.
+
+        Examples
+        --------
+        For this example we will use the simple Galilean translation operator
+
+        >>> from jax_quantity import Quantity
+        >>> import galax.coordinates.operators as gco
+        >>> import coordinax as cx
+
+        We can then create a spatial translation operator:
+
+        >>> op = gco.GalileanSpatialTranslationOperator(Quantity([1, 2, 3], "kpc"))
+        >>> op
+        GalileanSpatialTranslationOperator( translation=Cartesian3DVector( ... ) )
+
+        We can then apply the operator to a position:
+
+        >>> pos = cx.Cartesian3DVector.constructor(Quantity([1.0, 2.0, 3.0], "kpc"))
+        >>> t = Quantity(0.0, "Gyr")
+        >>> pos
+        Cartesian3DVector( ... )
+
+        >>> op(pos, t)
+        (Cartesian3DVector( ... ),
+         Quantity['time'](Array(0., dtype=float64, ...), unit='Gyr'))
+        """
         msg = "implement this method in the subclass"
         raise NotImplementedError(msg)
 
     @dispatch
+    def __call__(
+        self: "AbstractOperator",
+        q: Shaped[Quantity["length"], "*batch 3"],
+        t: Quantity["time"],
+        /,
+    ) -> tuple[Abstract3DVector, Quantity["time"]]:
+        """Apply the operator to the coordinates.
+
+        Examples
+        --------
+        >>> from jax_quantity import Quantity
+        >>> import galax.coordinates.operators as gco
+        >>> import coordinax as cx
+
+        We can then create a spatial translation operator:
+
+        >>> op = gco.GalileanSpatialTranslationOperator(Quantity([1, 2, 3], "kpc"))
+        >>> op
+        GalileanSpatialTranslationOperator( translation=Cartesian3DVector( ... ) )
+
+        We can then apply the operator to a position:
+
+        >>> pos = Quantity([1.0, 2.0, 3.0], "kpc")
+        >>> t = Quantity(0.0, "Gyr")
+
+        >>> op(pos, t)
+        (Cartesian3DVector( ... ),
+         Quantity['time'](Array(0., dtype=float64, ...), unit='Gyr'))
+        """
+        return self(Cartesian3DVector.constructor(q), t)
+
+    @dispatch
     def __call__(self: "AbstractOperator", x: FourVector, /) -> FourVector:
-        """Apply the operator to the coordinates."""
+        """Apply the operator to the coordinates.
+
+        Examples
+        --------
+        >>> from jax_quantity import Quantity
+        >>> import galax.coordinates.operators as gco
+        >>> import coordinax as cx
+
+        We can then create a spatial translation operator:
+
+        >>> op = gco.GalileanSpatialTranslationOperator(Quantity([1, 2, 3], "kpc"))
+        >>> op
+        GalileanSpatialTranslationOperator( translation=Cartesian3DVector( ... ) )
+
+        We can then apply the operator to a position:
+
+        >>> pos = cx.FourVector.constructor(Quantity([0, 1.0, 2.0, 3.0], "kpc"))
+        >>> pos
+        FourVector( t=Quantity[PhysicalType('time')](...), q=Cartesian3DVector( ... ) )
+
+        >>> newpos = op(pos)
+        >>> newpos
+        FourVector( t=Quantity[PhysicalType('time')](...), q=Cartesian3DVector( ... ) )
+        >>> newpos.q.x
+        Quantity['length'](Array(2., dtype=float64), unit='kpc')
+        """
         q, t = self(x.q, x.t)  # redispatch on (q, t)
         return FourVector(q=q, t=t)
+
+    @dispatch
+    def __call__(
+        self: "AbstractOperator", q: Shaped[Quantity["length"], "*#batch 4"], /
+    ) -> FourVector:
+        """Apply the operator to the coordinates.
+
+        Examples
+        --------
+        >>> from jax_quantity import Quantity
+        >>> import galax.coordinates.operators as gco
+        >>> import coordinax as cx
+
+        We can then create a spatial translation operator:
+
+        >>> op = gco.GalileanSpatialTranslationOperator(Quantity([1, 2, 3], "kpc"))
+        >>> op
+        GalileanSpatialTranslationOperator( translation=Cartesian3DVector( ... ) )
+
+        We can then apply the operator to a position:
+
+        >>> pos = Quantity([0, 1.0, 2.0, 3.0], "kpc")
+        >>> pos
+        Quantity['length'](Array([0., 1., 2., 3.], dtype=float64), unit='kpc')
+
+        >>> newpos = op(pos)
+        >>> newpos
+        FourVector( t=Quantity[PhysicalType('time')](...), q=Cartesian3DVector( ... ) )
+        >>> newpos.q.x
+        Quantity['length'](Array(2., dtype=float64), unit='kpc')
+        """
+        return self(FourVector.constructor(q))
 
     @dispatch
     def __call__(
@@ -93,7 +209,39 @@ class AbstractOperator(eqx.Module):  # type: ignore[misc]
         t: Quantity["time"],  # noqa: ARG002
         /,
     ) -> tuple[AbstractPhaseSpacePositionBase, Quantity["time"]]:
-        """Apply the operator to a phase-space position."""
+        """Apply the operator to the coordinates.
+
+        Examples
+        --------
+        >>> from jax_quantity import Quantity
+        >>> import galax.coordinates as gc
+        >>> import galax.coordinates.operators as gco
+        >>> import coordinax as cx
+
+        We can then create a spatial translation operator:
+
+        >>> op = gco.GalileanSpatialTranslationOperator(Quantity([1, 2, 3], "kpc"))
+        >>> op
+        GalileanSpatialTranslationOperator( translation=Cartesian3DVector( ... ) )
+
+        We can then apply the operator to a position:
+
+        >>> pos = gc.PhaseSpacePosition(q=Quantity([1, 2, 3], "kpc"),
+        ...                             p=Quantity([4, 5, 6], "km/s"))
+        >>> t = Quantity(0.0, "Gyr")
+        >>> pos
+        PhaseSpacePosition(
+            q=Cartesian3DVector( ... ), p=CartesianDifferential3D( ... ) )
+
+        >>> newpos, newt = op(pos, t)
+        >>> newpos, newt
+        (PhaseSpacePosition( q=Cartesian3DVector( ... ),
+                             p=CartesianDifferential3D( ... ) ),
+         Quantity['time'](Array(0., dtype=float64, ...), unit='Gyr'))
+
+        >>> newpos.q.x
+        Quantity['length'](Array(2., dtype=float64), unit='kpc')
+        """
         msg = "implement this method in the subclass"
         raise NotImplementedError(msg)
 
@@ -109,6 +257,42 @@ class AbstractOperator(eqx.Module):  # type: ignore[misc]
         method to avoid having to implement for both phase-space-time and
         phase-space positions.  Alternatively, they can implement this method
         directly to avoid redispatching.
+
+        Examples
+        --------
+        >>> from jax_quantity import Quantity
+        >>> import galax.coordinates as gc
+        >>> import galax.coordinates.operators as gco
+        >>> import coordinax as cx
+
+        We can then create a spatial translation operator:
+
+        >>> op = gco.GalileanSpatialTranslationOperator(Quantity([1, 2, 3], "kpc"))
+        >>> op
+        GalileanSpatialTranslationOperator( translation=Cartesian3DVector( ... ) )
+
+        We can then apply the operator to a position:
+
+        >>> pos = gc.PhaseSpaceTimePosition(q=Quantity([1, 2, 3], "kpc"),
+        ...                                 p=Quantity([4, 5, 6], "km/s"),
+        ...                                 t=Quantity(0.0, "Gyr"))
+        >>> pos
+        PhaseSpaceTimePosition(
+            q=Cartesian3DVector( ... ),
+            p=CartesianDifferential3D( ... ),
+            t=Quantity[PhysicalType('time')](value=f64[], unit=Unit("Gyr"))
+        )
+
+        >>> newpos = op(pos)
+        >>> newpos
+        PhaseSpaceTimePosition(
+            q=Cartesian3DVector( ... ),
+            p=CartesianDifferential3D( ... ),
+            t=Quantity[PhysicalType('time')](value=f64[], unit=Unit("Gyr"))
+        )
+
+        >>> newpos.q.x
+        Quantity['length'](Array(2., dtype=float64), unit='kpc')
         """
         # redispatch on (psp, t)
         psp, t = self(PhaseSpacePosition(q=x.q, p=x.p), x.t)

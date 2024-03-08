3e74bcc957b7d5697a1ce97d2a4c34a9619c4bf9
nstarman <nstarman@users.noreply.github.com>
2024-03-08 16:17:51 -0500

    fix: ensure operator compat registered with psp
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/coordinates/operators/__init__.py
+++ src/galax/coordinates/operators/__init__.py
@@ -3,10 +3,8 @@
 E.g. a translation.
 """
 
-from . import _compat, _rotating
-from ._compat import *
+from . import _rotating
 from ._rotating import *
 
 __all__: list[str] = []
-__all__ += _compat.__all__
 __all__ += _rotating.__all__

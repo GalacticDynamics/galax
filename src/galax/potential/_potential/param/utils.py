5c82841b80699ec0a176609dd15c68ec45aa494a
nstarman <nstarman@users.noreply.github.com>
2024-04-01 11:12:49 -0400

    refactor: only need all_parameters
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/potential/_potential/param/utils.py
+++ src/galax/potential/_potential/param/utils.py
@@ -12,12 +12,6 @@ if TYPE_CHECKING:
     from galax.potential._potential.param.field import ParameterField
 
 
-def all_cls_vars(obj: object | type, /) -> dict[str, Any]:
-    """Return all variables in the whole class hierarchy."""
-    cls = obj if inspect.isclass(obj) else type(obj)
-    return functools.reduce(operator.__or__, map(vars, cls.mro()[::-1]))
-
-
 def all_parameters(obj: object | type, /) -> dict[str, "ParameterField"]:
     """Get all fields of a dataclass, including those not-yet finalized.
 
@@ -32,11 +26,17 @@ def all_parameters(obj: object | type, /) -> dict[str, "ParameterField"]:
         All fields of the dataclass, including those not yet finalized in the class, if
         it's still under construction, e.g. in ``__init_subclass__``.
     """
-    from galax.potential._potential.param.field import ParameterField
+    from .field import ParameterField
 
+    # Get all the class variables
+    cls = obj if inspect.isclass(obj) else type(obj)
+    all_cls_vars: dict[str, Any]
+    all_cls_vars = functools.reduce(operator.__or__, map(vars, cls.mro()[::-1]))
+
+    # Get all the fields
     return {
         k: cast(ParameterField, v if isinstance(v, ParameterField) else v.default)
-        for k, v in all_cls_vars(obj).items()
+        for k, v in all_cls_vars.items()
         if (
             isinstance(v, ParameterField)
             or (isinstance(v, Field) and isinstance(v.default, ParameterField))

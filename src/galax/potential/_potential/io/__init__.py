f2383ba31a99281f0144270ec92eec4eadb0b28e
nstarman <nstarman@users.noreply.github.com>
2024-04-26 16:24:32 -0400

    refactor: make gala I/O lazy
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/potential/_potential/io/__init__.py
+++ src/galax/potential/_potential/io/__init__.py
@@ -6,12 +6,32 @@ Conversion is useful for e.g. converting a
 :class:`gala.potential.PotentialBase` object.
 """
 
-__all__: list[str] = ["gala_to_galax"]
+__all__ = ["gala_to_galax"]
 
+import sys
 
 from galax.utils._optional_deps import HAS_GALA
 
-if HAS_GALA:
-    from ._gala import gala_to_galax
-else:
-    from ._gala_noop import gala_to_galax  # type: ignore[assignment]
+
+def __dir__() -> list[str]:
+    """Return the list of names in the module."""
+    return sorted(__all__)
+
+
+def __getattr__(name: str) -> object:
+    """Get the attribute."""
+    match name:
+        case "gala_to_galax":
+            if HAS_GALA:
+                from ._gala import gala_to_galax as out
+            else:
+                from ._gala_noop import gala_to_galax as out
+
+        case _:
+            msg = f"module {__name__!r} has no attribute {name!r}"
+            raise AttributeError(msg)
+
+    # Cache the function in this module
+    setattr(sys.modules[__name__], name, out)
+
+    return out

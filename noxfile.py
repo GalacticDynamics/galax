bfa392881d0243f7d1cb4998e7de1dd65ebdcbb9
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-04-27 17:31:53 -0400

    build: bump from scientific-python/cookie update (#268)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,9 @@ import nox
 
 DIR = Path(__file__).parent.resolve()
 
+nox.needs_version = ">=2024.3.2"
 nox.options.sessions = ["lint", "tests", "doctests"]
+nox.options.default_venv_backend = "uv|virtualenv"
 
 
 @nox.session

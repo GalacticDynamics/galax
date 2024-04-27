5e66b3ae319a8a8b277983ff251681f740d7341f
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-04-27 16:35:14 -0400

    feat: psp gala compat (#266)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/dynamics/__init__.py
+++ src/galax/dynamics/__init__.py
@@ -4,10 +4,14 @@ from jaxtyping import install_import_hook
 from lazy_loader import attach_stub
 
 from galax.setup_package import RUNTIME_TYPECHECKER
+from galax.utils._optional_deps import HAS_GALA
 
 with install_import_hook("galax.dynamics", RUNTIME_TYPECHECKER):
     __getattr__, __dir__, __all__ = attach_stub(__name__, __file__)
 
+if HAS_GALA:
+    from . import _compat  # noqa: F401
+
 
 # Cleanup
-del install_import_hook, RUNTIME_TYPECHECKER
+del install_import_hook, RUNTIME_TYPECHECKER, HAS_GALA

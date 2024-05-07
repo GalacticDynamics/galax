04a45b9cebd23aa1dd87c1a673dbf87777b6d3d7
nstarman <nstarman@users.noreply.github.com>
2024-05-07 12:26:10 -0400

    build: opt dep check
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/utils/_optional_deps.py
+++ src/galax/utils/_optional_deps.py
@@ -5,6 +5,12 @@ __all__ = ["HAS_GALA", "GSL_ENABLED"]
 from importlib.util import find_spec
 
 HAS_GALA: bool = find_spec("gala") is not None
+if HAS_GALA:  # pragma: no cover  # TODO: remove this check
+    try:
+        import gala.dynamics  # noqa: F401
+    except Exception:  # noqa: BLE001
+        HAS_GALA = False
+
 
 GSL_ENABLED: bool
 if HAS_GALA:

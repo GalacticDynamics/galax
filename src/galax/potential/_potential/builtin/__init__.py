9b1a99644aa9472fdbdff46a6e92897ba2ebf31f
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-05-23 00:31:53 -0400

    refactor: sep out bars (#315)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/potential/_potential/builtin/__init__.py
+++ src/galax/potential/_potential/builtin/__init__.py
@@ -1,7 +1,8 @@
 """``galax`` Potentials."""
 # ruff:noqa: F401
 
-from . import builtin, logarithmic, nfw, special
+from . import bars, builtin, logarithmic, nfw, special
+from .bars import *
 from .builtin import *
 from .logarithmic import *
 from .nfw import *
@@ -9,6 +10,7 @@ from .special import *
 
 __all__: list[str] = []
 __all__ += builtin.__all__
+__all__ += bars.__all__
 __all__ += logarithmic.__all__
 __all__ += nfw.__all__
 __all__ += special.__all__

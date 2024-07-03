12f9d62f6bd74fa28dd5bf132500ae27cb2f8a5d
nstarman <nstarman@users.noreply.github.com>
2024-07-03 00:27:53 -0400

    refactor: namespace tidying
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/dynamics/_dynamics/integrate/__init__.py
+++ src/galax/dynamics/_dynamics/integrate/__init__.py
@@ -1,16 +1,3 @@
-"""galax: Galactic Dynamix in Jax."""
-
-from . import api, base, builtin, funcs
-from .api import *
-from .base import *
-from .builtin import *
-from .funcs import *
+"""Integration routines."""
 
 __all__: list[str] = []
-__all__ += api.__all__
-__all__ += base.__all__
-__all__ += builtin.__all__
-__all__ += funcs.__all__
-
-# Cleanup
-del api, base, builtin, funcs
--- src/galax/dynamics/integrate.py
+++ src/galax/dynamics/integrate.py
@@ -1,10 +1,16 @@
 """:mod:`galax.dynamics.integrate`."""
 
-from ._dynamics import integrate
-from ._dynamics.integrate import *  # noqa: F403
+from ._dynamics.integrate import api, base, builtin, funcs
+from ._dynamics.integrate.api import *  # noqa: F403
+from ._dynamics.integrate.base import *  # noqa: F403
+from ._dynamics.integrate.builtin import *  # noqa: F403
+from ._dynamics.integrate.funcs import *  # noqa: F403
 
 __all__: list[str] = []
-__all__ += integrate.__all__
+__all__ += api.__all__
+__all__ += base.__all__
+__all__ += builtin.__all__
+__all__ += funcs.__all__
 
 # Cleanup
-del integrate
+del api, base, builtin, funcs

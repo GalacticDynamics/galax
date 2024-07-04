652b5d4577be2406fcbd4c923e9ab0e43ed3011e
nstarman <nstarman@users.noreply.github.com>
2024-07-04 12:31:20 -0400

    refactor: simplify coordinates namespace
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/coordinates/_psp/__init__.py
+++ src/galax/coordinates/_psp/__init__.py
@@ -1,25 +1,3 @@
 """Phase-space positions."""
 
-from . import (
-    base,
-    base_composite,
-    base_psp,
-    core,
-    interp,
-    operator_compat,  # noqa: F401
-    utils,
-)
-from .base import *
-from .base_composite import *
-from .base_psp import *
-from .core import *
-from .interp import *
-from .utils import *
-
 __all__: list[str] = []
-__all__ += base.__all__
-__all__ += base_psp.__all__
-__all__ += base_composite.__all__
-__all__ += core.__all__
-__all__ += interp.__all__
-__all__ += utils.__all__

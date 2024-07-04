607b525c1c7e2d3b45d0debe6a472a340fd5bedb
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-07-04 12:42:07 -0400

    refactor: simplify coordinates namespace (#376)
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/coordinates/__init__.py
+++ src/galax/coordinates/__init__.py
@@ -9,13 +9,21 @@ from galax.setup_package import RUNTIME_TYPECHECKER
 from galax.utils._optional_deps import HAS_GALA
 
 with install_import_hook("galax.coordinates", RUNTIME_TYPECHECKER):
-    from . import operators
-    from ._psp import *
-
-from . import _psp  # only for __all__
+    from . import _psp, operators
+    from ._psp.base import *
+    from ._psp.base_composite import *
+    from ._psp.base_psp import *
+    from ._psp.core import *
+    from ._psp.interp import *
+    from ._psp.utils import *
 
 __all__: list[str] = ["operators"]
-__all__ += _psp.__all__
+__all__ += _psp.base.__all__
+__all__ += _psp.base_psp.__all__
+__all__ += _psp.base_composite.__all__
+__all__ += _psp.core.__all__
+__all__ += _psp.interp.__all__
+__all__ += _psp.utils.__all__
 
 # Clean up the namespace
 del install_import_hook, RUNTIME_TYPECHECKER, HAS_GALA, _psp

2b69cd1ec0151309293d916276db8d454552a7a5
nstarman <nstarman@users.noreply.github.com>
2024-07-08 12:51:35 -0400

    test: Immutable dict or
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- tests/unit/utils/test_collections.py
+++ tests/unit/utils/test_collections.py
@@ -86,6 +86,10 @@ class TestImmutableDict:
         assert d | OrderedDict([("c", 3)]) == ImmutableDict(a=1, b=2, c=3)
         assert d | MappingProxyType({"c": 3}) == ImmutableDict(a=1, b=2, c=3)
 
+        # Should raise TypeError if not a mapping.
+        with pytest.raises(TypeError, match="unsupported operand type"):
+            _ = d | 1
+
     def test_ror(self, d: ImmutableDict[str, Any]) -> None:
         """Test `__ror__`."""
         # Reverse order

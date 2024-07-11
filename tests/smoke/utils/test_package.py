34c77aab00a1087d7eab1730b901611a5289ec86
nstarman <nstarman@users.noreply.github.com>
2024-07-10 23:39:05 -0400

    test: rm collections
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- tests/smoke/utils/test_package.py
+++ tests/smoke/utils/test_package.py
@@ -3,8 +3,4 @@ from galax import utils
 
 def test__all__() -> None:
     """Test that `galax.utils` has the expected `__all__`."""
-    assert utils.__all__ == [
-        "dataclasses",
-        *utils._jax.__all__,
-        *utils._collections.__all__,
-    ]
+    assert utils.__all__ == ["dataclasses", *utils._jax.__all__]

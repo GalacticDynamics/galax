5c299119c99171b370febca0d08522e7f1a2791c
nstarman <nstarman@users.noreply.github.com>
2024-08-09 13:57:14 +0100

    test: fix
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/dynamics/_dynamics/integrate/__init__.py
+++ src/galax/dynamics/_dynamics/integrate/__init__.py
@@ -1,18 +1,14 @@
 """galax: Galactic Dynamix in Jax."""
 
-from . import api, base, builtin, funcs, utils
-from .api import *
-from .base import *
-from .builtin import *
+from . import core, funcs, utils
+from .core import *
 from .funcs import *
 from .utils import *
 
 __all__: list[str] = []
-__all__ += api.__all__
-__all__ += base.__all__
-__all__ += builtin.__all__
+__all__ += core.__all__
 __all__ += funcs.__all__
 __all__ += utils.__all__
 
 # Cleanup
-del api, base, builtin, funcs, utils
+del core, funcs, utils

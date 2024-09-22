2aa7eb6d04ceed74a3d0031f54936d6778db0d4c
nstarman <nstarman@users.noreply.github.com>
2024-09-21 21:12:59 -0400

    feat: orbit plotting
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- src/galax/_interop/matplotlib/__init__.py
+++ src/galax/_interop/matplotlib/__init__.py
@@ -4,6 +4,9 @@ __all__ = [
     # potential
     "plot_potential_contours",
     "plot_density_contours",
+    # orbit
+    "plot_components",
 ]
 
+from .orbit import plot_components
 from .potential import plot_density_contours, plot_potential_contours

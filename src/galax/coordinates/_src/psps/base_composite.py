c1adbf771b991c3258ca7aaa3cf050ffdce8f3f1
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-10-27 13:41:04 -0400

    fix: f-strings in error message (#488)

--- src/galax/coordinates/_src/psps/base_composite.py
+++ src/galax/coordinates/_src/psps/base_composite.py
@@ -368,7 +368,7 @@ def replace(
     """
     # TODO: directly call the Mapping implementation
     extra_keys = set(kwargs) - set(obj)
-    kwargs = eqx.error_if(kwargs, any(extra_keys), "invalid keys {extra_keys}.")
+    kwargs = eqx.error_if(kwargs, any(extra_keys), f"invalid keys {extra_keys}.")
 
     return type(obj)(**{**obj, **kwargs})
 

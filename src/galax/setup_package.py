ec5092ccd1d9a49479dce7b0c0dd06e718b16232
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-12-29 10:36:28 -0800

    🚩 flag(jaxtyping): update the runtime typechecking flag

--- src/galax/setup_package.py
+++ src/galax/setup_package.py
@@ -4,13 +4,24 @@ Copyright (c) 2023 galax maintainers. All rights reserved.
 """
 
 import os
+from typing import Final
 
 from jax import config
 
 config.update("jax_enable_x64", True)  # noqa: FBT003
 
-RUNTIME_TYPECHECKER = (
-    "beartype.beartype"
-    if (os.environ.get("GALAX_ENABLE_RUNTIME_TYPECHECKS", "0") == "1")
+
+RUNTIME_TYPECHECKER: Final[str | None] = (
+    v
+    if (v := os.environ.get("GALAX_ENABLE_RUNTIME_TYPECHECKING", None)) != "None"
     else None
 )
+"""Runtime type checking variable "GALAX_ENABLE_RUNTIME_TYPECHECKING".
+
+Set to "None" to disable runtime typechecking (default). Set to
+"beartype.beartype" to enable runtime typechecking.
+
+See https://docs.kidger.site/jaxtyping/api/runtime-type-checking for more
+information on options.
+
+"""

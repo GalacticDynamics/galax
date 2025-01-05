d2dc19ed146ccaf653ccba52ad27b4c0a9e96c32
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-04 19:29:03 -0500

    🎨 style(coordinates): improve imports for operators

--- src/galax/coordinates/ops.py
+++ src/galax/coordinates/ops.py
@@ -3,11 +3,6 @@
 E.g. a translation.
 """
 
-from ._src.ops import rotating
-from ._src.ops.rotating import *  # noqa: F403
+__all__ = ["ConstantRotationZOperator"]
 
-__all__: list[str] = []
-__all__ += rotating.__all__
-
-# Clean up the namespace
-del rotating
+from ._src.ops.rotating import ConstantRotationZOperator

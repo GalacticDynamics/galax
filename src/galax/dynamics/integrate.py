eea779c1746a3ea4a41d4086f418163f4b44a85e
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-08 23:17:02 -0500

    🎨 style(dynamics): clean up imports
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/galax/dynamics/integrate.py
+++ src/galax/dynamics/integrate.py
@@ -1,16 +1,17 @@
 """:mod:`galax.dynamics.integrate`."""
 
-from ._src.integrate import funcs, integrator, type_hints, utils
-from ._src.integrate.funcs import *  # noqa: F403
-from ._src.integrate.integrator import *  # noqa: F403
-from ._src.integrate.type_hints import *  # noqa: F403
-from ._src.integrate.utils import *  # noqa: F403
+__all__ = [
+    "evaluate_orbit",
+    "Integrator",
+    "Interpolant",
+    "VectorField",
+    "parse_time_specification",
+]
 
-__all__: list[str] = []
-__all__ += integrator.__all__
-__all__ += funcs.__all__
-__all__ += utils.__all__
-__all__ += type_hints.__all__
-
-# Cleanup
-del integrator, funcs, utils, type_hints
+from ._src.integrate import (
+    Integrator,
+    Interpolant,
+    VectorField,
+    evaluate_orbit,
+    parse_time_specification,
+)

db8e08d0b357fec7115bb5b03dedf027c5ae8208
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-13 10:36:33 -0500

    🎨 style(coordinates): improve import structure (#537)

--- src/galax/coordinates/__init__.py
+++ src/galax/coordinates/__init__.py
@@ -9,24 +9,32 @@ from galax.setup_package import RUNTIME_TYPECHECKER
 
 with install_import_hook("galax.coordinates", RUNTIME_TYPECHECKER):
     from . import ops
-    from ._src import psps
-    from ._src.psps import operator_compat
-    from ._src.psps.base import *
-    from ._src.psps.base_composite import *
-    from ._src.psps.base_psp import *
-    from ._src.psps.core import *
-    from ._src.psps.core_composite import *
-    from ._src.psps.interp import *
-    from ._src.psps.utils import *
+    from ._src.psps import (
+        AbstractBasePhaseSpacePosition,
+        AbstractCompositePhaseSpacePosition,
+        AbstractPhaseSpacePosition,
+        ComponentShapeTuple,
+        CompositePhaseSpacePosition,
+        InterpolatedPhaseSpacePosition,
+        PhaseSpacePosition,
+        PhaseSpacePositionInterpolant,
+    )
 
-__all__: list[str] = ["ops"]
-__all__ += psps.base.__all__
-__all__ += psps.base_psp.__all__
-__all__ += psps.base_composite.__all__
-__all__ += psps.core.__all__
-__all__ += psps.core_composite.__all__
-__all__ += psps.interp.__all__
-__all__ += psps.utils.__all__
+__all__ = [
+    # Modules
+    "ops",
+    # Contents
+    "AbstractBasePhaseSpacePosition",
+    "AbstractPhaseSpacePosition",
+    "PhaseSpacePosition",
+    "AbstractCompositePhaseSpacePosition",
+    "CompositePhaseSpacePosition",
+    # Interpolation
+    "InterpolatedPhaseSpacePosition",
+    "PhaseSpacePositionInterpolant",
+    # Utils
+    "ComponentShapeTuple",
+]
 
 # Clean up the namespace
-del install_import_hook, RUNTIME_TYPECHECKER, psps, operator_compat
+del install_import_hook, RUNTIME_TYPECHECKER

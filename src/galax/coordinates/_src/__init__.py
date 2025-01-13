f07f5ca4053f485b89bb0d4194385b30058baf87
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-13 10:12:09 -0500

    🎨 style(coordinates): improve import structure

--- src/galax/coordinates/_src/__init__.py
+++ src/galax/coordinates/_src/__init__.py
@@ -1,3 +1,10 @@
-"""Galax coordinates package."""
+"""Galax coordinates package.
+
+Private API.
+
+"""
 
 __all__: list[str] = []
+
+# Register by importing
+from .psps import operator_compat  # noqa: F401
--- src/galax/coordinates/_src/psps/__init__.py
+++ src/galax/coordinates/_src/psps/__init__.py
@@ -1,3 +1,25 @@
-"""Phase-space positions."""
+"""Phase-space positions.
 
-__all__: list[str] = []
+This is private API.
+
+"""
+
+__all__ = [
+    "AbstractBasePhaseSpacePosition",
+    "AbstractPhaseSpacePosition",
+    "PhaseSpacePosition",
+    "AbstractCompositePhaseSpacePosition",
+    "CompositePhaseSpacePosition",
+    # Interpolation
+    "InterpolatedPhaseSpacePosition",
+    "PhaseSpacePositionInterpolant",
+    # Utils
+    "ComponentShapeTuple",
+]
+
+from .base import AbstractBasePhaseSpacePosition, ComponentShapeTuple
+from .base_composite import AbstractCompositePhaseSpacePosition
+from .base_psp import AbstractPhaseSpacePosition
+from .core import PhaseSpacePosition
+from .core_composite import CompositePhaseSpacePosition
+from .interp import InterpolatedPhaseSpacePosition, PhaseSpacePositionInterpolant

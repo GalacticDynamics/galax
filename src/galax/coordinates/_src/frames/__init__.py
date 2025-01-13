868f137866b494078af3baaabdfb9333d0438021
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-13 09:47:07 -0500

    ✨ feat(coordinates): start simulation frames
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

---
+++ src/galax/coordinates/_src/frames/__init__.py
@@ -0,0 +1,9 @@
+"""Reference Frames.
+
+Building off of `coordinax.frames`.
+
+"""
+
+__all__ = ["SimulationFrame"]
+
+from .simulation import SimulationFrame
---
+++ src/galax/coordinates/frames.py
@@ -0,0 +1,8 @@
+"""Transformations of the coordinate reference frame.
+
+E.g. a translation.
+"""
+
+__all__ = ["SimulationFrame"]
+
+from ._src.frames.simulation import SimulationFrame

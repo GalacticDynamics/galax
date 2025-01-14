9c4c158598430db2d9339c9fbd35d23b0adfbfba
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-13 22:36:46 -0500

    ♻️ refactor(dynamics): rename private files (#545)

--- src/galax/dynamics/_src/fields/hamiltonian.py
+++ src/galax/dynamics/_src/fields/hamiltonian.py
@@ -4,50 +4,22 @@ This is private API.
 
 """
 
-__all__ = ["AbstractDynamicsField", "HamiltonianField"]
+__all__ = ["HamiltonianField"]
 
-import abc
 from functools import partial
 from typing import Any, final
 
-import diffrax
-import equinox as eqx
 import jax
-from jaxtyping import PyTree, Shaped
 from plum import convert, dispatch
 
 import coordinax as cx
 import unxt as u
-from unxt.quantity import AbstractQuantity, UncheckedQuantity as FastQ
+from unxt.quantity import UncheckedQuantity as FastQ
 
 import galax.coordinates as gc
 import galax.potential as gp
 import galax.typing as gt
-
-
-class AbstractDynamicsField(eqx.Module, strict=True):  # type: ignore[misc,call-arg]
-    """ABC for dynamics fields.
-
-    Note that this provides a default implementation for the `terms` property,
-    which is a jitted `diffrax.ODETerm` object. This is a convenience for the
-    user and may be overridden, e.g. to support an SDE or other differential
-    equation types.
-
-    """
-
-    @abc.abstractmethod
-    def __call__(
-        self, t: Any, qp: tuple[Any, Any], args: tuple[Any, ...], /
-    ) -> tuple[Any, Any]:
-        raise NotImplementedError  # pragma: no cover
-
-    @property
-    def terms(self) -> PyTree[diffrax.AbstractTerm]:
-        """Return the AbstractTerm."""
-        return diffrax.ODETerm(jax.jit(self.__call__))
-
-
-##############################################################################
+from .base import AbstractDynamicsField
 
 
 @final
@@ -98,7 +70,7 @@ class HamiltonianField(AbstractDynamicsField, strict=True):  # type: ignore[call
 @partial(jax.jit, inline=True)
 def call(
     self: HamiltonianField,
-    t: Shaped[AbstractQuantity, "*#batch"],
+    t: gt.RealQScalar,
     q: gt.BatchableQ,
     p: gt.BatchableP,
     args: tuple[Any, ...] | None,  # noqa: ARG001
@@ -133,7 +105,7 @@ def call(
 @partial(jax.jit, inline=True)
 def call(
     self: HamiltonianField,
-    t: Shaped[AbstractQuantity, "*#batch"],
+    t: gt.RealQScalar,
     qp: gt.BatchableQP,
     args: tuple[Any, ...] | None,
     /,

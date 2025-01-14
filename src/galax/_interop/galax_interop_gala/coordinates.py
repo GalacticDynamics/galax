72efeb349bd6e8878d45dcbd3e45c8261caa1786
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-14 17:30:59 -0500

    🎨 style(imports): standardize galax imports
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/galax/_interop/galax_interop_gala/coordinates.py
+++ src/galax/_interop/galax_interop_gala/coordinates.py
@@ -5,28 +5,31 @@ __all__: list[str] = []
 import warnings
 from typing import Any
 
-import gala.dynamics as gd
+import gala.dynamics as galad
 from astropy.coordinates import BaseDifferential, BaseRepresentation
 from plum import conversion_method, convert
 
-import galax.coordinates as gcx
+import galax.coordinates as gc
 
 # --------------------------------------------------
 # Gala to Galax
 
 
-@conversion_method(type_from=gd.PhaseSpacePosition, type_to=gcx.PhaseSpacePosition)  # type: ignore[arg-type]
+@conversion_method(type_from=galad.PhaseSpacePosition, type_to=gc.PhaseSpacePosition)  # type: ignore[arg-type]
 def gala_psp_to_galax_psp(
-    obj: gd.PhaseSpacePosition, /, t: Any | None = None
-) -> gcx.PhaseSpacePosition:
+    obj: galad.PhaseSpacePosition, /, t: Any | None = None
+) -> gc.PhaseSpacePosition:
     """Gala to galax ``PhaseSpacePosition``."""
-    return gcx.PhaseSpacePosition(q=obj.pos, p=obj.vel, t=t)
+    return gc.PhaseSpacePosition(q=obj.pos, p=obj.vel, t=t)
 
 
-@gcx.PhaseSpacePosition.from_.dispatch  # type: ignore[attr-defined,misc]
+@gc.PhaseSpacePosition.from_.dispatch  # type: ignore[attr-defined,misc]
 def from_(
-    _: type[gcx.PhaseSpacePosition], obj: gd.PhaseSpacePosition, /, t: Any | None = None
-) -> gcx.PhaseSpacePosition:
+    _: type[gc.PhaseSpacePosition],
+    obj: galad.PhaseSpacePosition,
+    /,
+    t: Any | None = None,
+) -> gc.PhaseSpacePosition:
     """Construct a :mod:`galax` PhaseSpacePosition from a :mod:`gala` one."""
     return gala_psp_to_galax_psp(obj, t=t)  # type: ignore[call-arg]
 
@@ -35,8 +38,8 @@ def from_(
 # Galax to Gala
 
 
-@conversion_method(type_from=gcx.PhaseSpacePosition, type_to=gd.PhaseSpacePosition)  # type: ignore[arg-type]
-def galax_psp_to_gala_psp(obj: gcx.PhaseSpacePosition, /) -> gd.PhaseSpacePosition:
+@conversion_method(type_from=gc.PhaseSpacePosition, type_to=galad.PhaseSpacePosition)  # type: ignore[arg-type]
+def galax_psp_to_gala_psp(obj: gc.PhaseSpacePosition, /) -> galad.PhaseSpacePosition:
     """Galax to gala ``PhaseSpacePosition``.
 
     .. warning::
@@ -46,7 +49,7 @@ def galax_psp_to_gala_psp(obj: gcx.PhaseSpacePosition, /) -> gd.PhaseSpacePositi
     """
     warnings.warn("The time is not preserved in the conversion!", stacklevel=2)
 
-    return gd.PhaseSpacePosition(
+    return galad.PhaseSpacePosition(
         pos=convert(obj.q, BaseRepresentation),
         vel=convert(obj.p, BaseDifferential),
     )

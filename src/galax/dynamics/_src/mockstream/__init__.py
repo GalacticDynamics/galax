74a7c5de75a477a0e55ac29b88dc327844fb9327
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-15 00:44:45 -0500

    ✨ feat(dynamics): runtime type checking (#555)
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/galax/dynamics/_src/mockstream/__init__.py
+++ src/galax/dynamics/_src/mockstream/__init__.py
@@ -1,14 +1,10 @@
-"""galax: Galactic Dynamix in Jax."""
+"""Mockstream.
 
-from . import core, df, mockstream_generator
-from .core import *
-from .df import *
-from .mockstream_generator import *
+This is private API.
 
-__all__: list[str] = []
-__all__ += df.__all__
-__all__ += core.__all__
-__all__ += mockstream_generator.__all__
+"""
 
-# Cleanup
-del core, df, mockstream_generator
+__all__ = ["MockStreamGenerator", "MockStreamArm", "MockStream"]
+
+from .core import MockStream, MockStreamArm
+from .mockstream_generator import MockStreamGenerator

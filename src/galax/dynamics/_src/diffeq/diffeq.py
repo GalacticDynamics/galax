8dbac291ae175fcb093789faad7e807fdf9d4d76
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-01-17 15:52:34 -0500

    ✨ feat(dynamics): vectorize dynamics solver (#575)
    * 🎨 style(dynamics): diffeq vectorization
    * ✨ feat(dynamics): vectorize dynamics solver

--- src/galax/dynamics/_src/diffeq/diffeq.py
+++ src/galax/dynamics/_src/diffeq/diffeq.py
@@ -54,21 +54,54 @@ class DiffEqSolver(eqx.Module, strict=True):  # type: ignore[call-arg,misc]
 
     Examples
     --------
+    >>> import jax.numpy as jnp
     >>> from galax.dynamics.integrate import DiffEqSolver
 
+    Construct a solver object.
+
     >>> solver = DiffEqSolver(dfx.Dopri5(),
-    ...                       stepsize_controller=dfx.PIDController(rtol=1e-5, atol=1e-5))
-    >>> saveat = dfx.SaveAt(ts=[0., 1., 2., 3.])
+    ...                stepsize_controller=dfx.PIDController(rtol=1e-5, atol=1e-5))
+
+    And a differential equation to solve.
+
     >>> term = dfx.ODETerm(lambda t, y, args: -y)
-    >>> sol = solver(term, t0=0, t1=3, dt0=0.1, y0=1, saveat=saveat)
 
-    >>> print(sol.ts)
-    [0. 1. 2. 3.]
+    Then solve the differential equation.
 
-    >>> print(sol.ys)
-    [1. 0.36788338 0.13533922 0.04978961]
+    >>> soln = solver(term, t0=0, t1=3, dt0=0.1, y0=1)
+    >>> soln
+    Solution( t0=f64[], t1=f64[], ts=f64[1],
+              ys=f64[1], ... )
 
-    """  # noqa: E501
+    The solution can be saved at specific times.
+
+    >>> saveat = dfx.SaveAt(ts=[0., 1., 2., 3.])
+    >>> soln = solver(term, t0=0, t1=3, dt0=0.1, y0=1, saveat=saveat)
+    >>> soln
+    Solution( t0=f64[], t1=f64[], ts=f64[4],
+              ys=f64[4], ... )
+
+    The solution can be densely interpolated.
+
+    >>> saveat = dfx.SaveAt(t1=True, dense=True)
+    >>> soln = solver(term, t0=0, t1=3, dt0=0.1, y0=1, saveat=saveat)
+    >>> soln
+    Solution( t0=f64[], t1=f64[], ts=f64[1],
+              ys=f64[1], ... )
+    >>> soln.evaluate(0.5)
+    Array(0.60653213, dtype=float64)
+
+    Using the `VectorizedDenseInterpolation` class, the interpolation can be
+    vectorized, enabling evaluation of batched solutions over batches of times.
+
+    >>> from galax.dynamics.integrate import VectorizedDenseInterpolation
+    >>> soln = solver(term, t0=0, t1=3, dt0=0.1, y0=1, saveat=saveat)
+    >>> soln = VectorizedDenseInterpolation.apply_to_solution(soln)
+    >>> soln.evaluate(jnp.array([0.1, 0.2, 0.3, 0.4]).reshape(2, 2))
+    Array([[0.90483742, 0.81872516],
+           [0.74080871, 0.67031456]], dtype=float64)
+
+    """
 
     #: The solver for the differential equation.
     #: See the diffrax guide on how to choose a solver.

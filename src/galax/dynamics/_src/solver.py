35473ce8fe3921ed4ff06f35de76ba059556f9bf
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-02-07 14:29:32 -0500

    ✨ feat(dynamics): add SolveState

--- src/galax/dynamics/_src/solver.py
+++ src/galax/dynamics/_src/solver.py
@@ -4,15 +4,73 @@ This is private API.
 
 """
 
-__all__ = ["AbstractSolver"]
+__all__ = ["AbstractSolver", "SolveState"]
 
 
 import abc
 from collections.abc import Mapping
-from typing import Any
+from typing import Any, TypeAlias
 
+import diffrax as dfx
 import equinox as eqx
+from jaxtyping import Array, PyTree
 from plum import dispatch
+
+import unxt as u
+
+DenseInfo: TypeAlias = dict[str, PyTree[Array]]
+
+
+class SolveState(eqx.Module, strict=True):  # type: ignore[misc, call-arg]
+    """State of the solver.
+
+    This is used as the return value for `galax.dynamics.AbstractSolver.init`
+    and `galax.dynamics.AbstractSolver.step`. It is used as the argument to
+    `galax.dynamics.AbstractSolver.step`, `galax.dynamics.AbstractSolver.run`,
+    and can also be passed to `galax.dynamics.AbstractSolver.solve`.
+
+    """
+
+    #: Current time.
+    t: Any
+    # ---- diffrax step outputs ----
+    #: Current solution at `t`.
+    y: PyTree
+    # TODO: figure out how to extract this from `diffrax.Solution`
+    # #: A local error estimate made during the step
+    # err: PyTree | None  # noqa: ERA001
+    # TODO: figure out how to extract this from `diffrax.Solution`
+    # #: Save information. This is a dictionary of information that is passed to
+    # # the solver's interpolation routine to calculate dense output. (Used with
+    # # SaveAt(ts=...) or SaveAt(dense=...).)
+    # save_info: DenseInfo  # noqa: ERA001
+    #: The value of the solver state at t1.
+    solver_state: Any
+    #: Step success. An integer (corresponding to diffrax.RESULTS) indicating
+    # whether the step happened successfully, or if (unusually) it failed for
+    # some reason.
+    success: dfx.RESULTS
+
+    # --- reconstruction info ---
+    units: u.AbstractUnitSystem
+
+    @classmethod
+    def from_step_output(
+        cls,
+        t: Any,
+        obj: tuple[PyTree, PyTree | None, DenseInfo, Any, dfx.RESULTS],
+        units: u.AbstractUnitSystem,
+        /,
+    ) -> "SolveState":
+        return cls(
+            t=t,
+            y=obj[0],
+            # err=obj[1],  # noqa: ERA001
+            # save_info=obj[2],  # noqa: ERA001
+            solver_state=obj[3],
+            success=obj[4],
+            units=units,
+        )
 
 
 class AbstractSolver(eqx.Module, strict=True):  # type: ignore[call-arg,misc]
@@ -26,12 +84,19 @@ class AbstractSolver(eqx.Module, strict=True):  # type: ignore[call-arg,misc]
     """
 
     @abc.abstractmethod
-    def init(self, *args: Any, **kwargs: Any) -> Any:
+    def init(self, *args: Any, **kwargs: Any) -> SolveState:
         """Initialize the solver."""
         raise NotImplementedError
 
     @abc.abstractmethod
-    def step(self, *args: Any, **kwargs: Any) -> Any:
+    def step(
+        self,
+        terms: Any,
+        state: SolveState,
+        t1: Any,
+        args: PyTree,
+        **step_kwargs: Any,  # e.g. solver_state, made_jump
+    ) -> SolveState:
         """Step the solver."""
         raise NotImplementedError
 

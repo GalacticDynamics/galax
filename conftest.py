fd433407dd0beef99c7366219d9fc9a09aa96133
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-02-07 13:28:28 -0500

    🔧 config(sybil): add markdown doc tests

--- conftest.py
+++ conftest.py
@@ -3,22 +3,32 @@
 from doctest import ELLIPSIS, NORMALIZE_WHITESPACE
 
 from sybil import Sybil
-from sybil.parsers import rest
+from sybil.parsers import myst, rest
 
 from optional_dependencies import OptionalDependencyEnum, auto
 from optional_dependencies.utils import chain_checks, get_version, is_installed
 
 optionflags = ELLIPSIS | NORMALIZE_WHITESPACE
 
-parsers = [
-    rest.DocTestParser(optionflags=optionflags),
-    rest.PythonCodeBlockParser(),
-    rest.SkipParser(),
-]
 
-python = Sybil(parsers=parsers, patterns=["*.rst", "*.py"])
+docs = Sybil(
+    parsers=[
+        myst.DocTestDirectiveParser(optionflags=optionflags),
+        myst.PythonCodeBlockParser(doctest_optionflags=optionflags),
+        myst.SkipParser(),
+    ],
+    patterns=["*.md"],
+)
+python = Sybil(
+    parsers=[
+        rest.DocTestParser(optionflags=optionflags),
+        rest.PythonCodeBlockParser(),
+        rest.SkipParser(),
+    ],
+    patterns=["*.rst", "*.py"],
+)
 
-pytest_collect_file = python.pytest()
+pytest_collect_file = (docs + python).pytest()
 
 
 class OptDeps(OptionalDependencyEnum):

248ef110b08c5d73bb6c6d9c30f79b5b2710e72b
Adrian Price-Whelan <adrian.prw@gmail.com>
2025-02-13 09:59:11 -0500

    Add more dispatches for lagrange_points function (#650)
    * add more dispatches for lagrange points function
    * convert to quantity first
    * add doctests

--- src/galax/dynamics/_src/cluster/register_funcs.py
+++ src/galax/dynamics/_src/cluster/register_funcs.py
@@ -5,25 +5,25 @@ __all__: list[str] = []
 from functools import partial
 
 import jax
-from plum import dispatch
+from plum import convert, dispatch
 
 import coordinax as cx
 import quaxed.numpy as jnp
 import unxt as u
 
+import galax.coordinates as gc
 import galax.potential as gp
 import galax.typing as gt
 from .api import L1L2LagrangePoints
 from .radius import tidal_radius_king1962
 
 
-# TODO: vec, space, coordinate, PSP I/O
 @dispatch
 @partial(jax.jit)
 def lagrange_points(
     potential: gp.AbstractPotential,
-    x: gt.LengthSz3,
-    v: gt.SpeedSz3,
+    x: gt.LengthSz3 | cx.vecs.AbstractPos3D,
+    v: gt.SpeedSz3 | cx.vecs.AbstractVel3D,
     /,
     *,
     mass: gt.MassSz0,
@@ -48,6 +48,7 @@ def lagrange_points(
     --------
     >>> import unxt as u
     >>> import galax.potential as gp
+    >>> from galax.dynamics.cluster import lagrange_points
 
     >>> pot = gp.MilkyWayPotential()
     >>> x = u.Quantity([8.0, 0.0, 0.0], "kpc")
@@ -61,12 +62,172 @@ def lagrange_points(
     >>> lpts.l2
     Quantity['length'](Array([8.02929074, 0. , 0. ], dtype=float64), unit='kpc')
 
+    This also works with `coordinax` vectors:
+
+    >>> import coordinax as cx
+    >>> lpts2 = lagrange_points(
+    ...     pot, cx.CartesianPos3D.from_(x), cx.CartesianVel3D.from_(v), mass=mass, t=t
+    ... )
+    >>> lpts2.l1
+    Quantity['length'](Array([7.97070926, 0. , 0. ], dtype=float64), unit='kpc')
+    >>> lpts2.l2
+    Quantity['length'](Array([8.02929074, 0. , 0. ], dtype=float64), unit='kpc')
+
     """
+    x = convert(x, u.Quantity)
+    v = convert(v, u.Quantity)
     r_t = tidal_radius_king1962(potential, x, v, mass=mass, t=t)
     r_hat = cx.vecs.normalize_vector(x)
     l1 = x - r_hat * r_t  # close
     l2 = x + r_hat * r_t  # far
     return L1L2LagrangePoints(l1=l1, l2=l2)
+
+
+@dispatch
+def lagrange_points(
+    pot: gp.AbstractPotential,
+    space: cx.Space,
+    /,
+    *,
+    mass: gt.MassSz0,
+    t: gt.TimeSz0,
+) -> L1L2LagrangePoints:
+    """Compute the lagrange points of a cluster in a host potential.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> import galax.potential as gp
+    >>> from galax.dynamics.cluster import lagrange_points
+
+    >>> pot = gp.MilkyWayPotential()
+    >>> x = cx.CartesianPos3D.from_(u.Quantity([8.0, 0.0, 0.0], "kpc"))
+    >>> v = cx.CartesianVel3D.from_(u.Quantity([0.0, 220.0, 0.0], "km/s"))
+    >>> mass = u.Quantity(1e4, "Msun")
+    >>> t = u.Quantity(0.0, "Gyr")
+
+    This also works with a `coordinax.Space` object:
+
+    >>> import coordinax as cx
+    >>> space = cx.Space(length=x, speed=v)
+    >>> lpts = lagrange_points(pot, space, mass=mass, t=t)
+    >>> lpts.l1
+    Quantity['length'](Array([7.97070926, 0. , 0. ], dtype=float64), unit='kpc')
+    >>> lpts.l2
+    Quantity['length'](Array([8.02929074, 0. , 0. ], dtype=float64), unit='kpc')
+
+    """
+    return lagrange_points(pot, space["length"], space["speed"], mass=mass, t=t)
+
+
+@dispatch
+def lagrange_points(
+    pot: gp.AbstractPotential,
+    coord: cx.frames.AbstractCoordinate,
+    /,
+    *,
+    mass: gt.MassSz0,
+    t: gt.TimeSz0,
+) -> L1L2LagrangePoints:
+    """Compute the lagrange points of a cluster in a host potential.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> import galax.potential as gp
+    >>> from galax.dynamics.cluster import lagrange_points
+
+    >>> pot = gp.MilkyWayPotential()
+    >>> x = cx.CartesianPos3D.from_(u.Quantity([8.0, 0.0, 0.0], "kpc"))
+    >>> v = cx.CartesianVel3D.from_(u.Quantity([0.0, 220.0, 0.0], "km/s"))
+    >>> mass = u.Quantity(1e4, "Msun")
+    >>> t = u.Quantity(0.0, "Gyr")
+
+    This also works with a `coordinax.Coordinate` object:
+
+    >>> import coordinax as cx
+    >>> space = cx.Coordinate(cx.Space(length=x, speed=v), frame=cx.frames.ICRS())
+    >>> lpts = lagrange_points(pot, space, mass=mass, t=t)
+    >>> lpts.l1
+    Quantity['length'](Array([7.97070926, 0. , 0. ], dtype=float64), unit='kpc')
+    >>> lpts.l2
+    Quantity['length'](Array([8.02929074, 0. , 0. ], dtype=float64), unit='kpc')
+
+    """
+    return lagrange_points(pot, coord.data, mass=mass, t=t)
+
+
+@dispatch
+def lagrange_points(
+    pot: gp.AbstractPotential,
+    w: gc.AbstractPhaseSpaceCoordinate,
+    /,
+    *,
+    mass: gt.MassSz0,
+) -> L1L2LagrangePoints:
+    """Compute the lagrange points of a cluster in a host potential.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> import galax.potential as gp
+    >>> from galax.dynamics.cluster import lagrange_points
+
+    >>> pot = gp.MilkyWayPotential()
+    >>> x = cx.CartesianPos3D.from_(u.Quantity([8.0, 0.0, 0.0], "kpc"))
+    >>> v = cx.CartesianVel3D.from_(u.Quantity([0.0, 220.0, 0.0], "km/s"))
+    >>> mass = u.Quantity(1e4, "Msun")
+    >>> t = u.Quantity(0.0, "Gyr")
+
+    This also works with a `galax.coordinates.PhaseSpaceCoordinate` object:
+
+    >>> import galax.coordinates as gc
+    >>> w = gc.PhaseSpaceCoordinate(q=x, p=v, t=t)
+    >>> lpts = lagrange_points(pot, w, mass=mass)
+    >>> lpts.l1
+    Quantity['length'](Array([7.97070926, 0. , 0. ], dtype=float64), unit='kpc')
+    >>> lpts.l2
+    Quantity['length'](Array([8.02929074, 0. , 0. ], dtype=float64), unit='kpc')
+
+    """
+    return lagrange_points(pot, w.q, w.p, mass=mass, t=w.t.squeeze())
+
+
+@dispatch
+def lagrange_points(
+    pot: gp.AbstractPotential,
+    w: gc.PhaseSpacePosition,
+    /,
+    *,
+    mass: gt.MassSz0,
+    time: gt.TimeSz0,
+) -> L1L2LagrangePoints:
+    """Compute the lagrange points of a cluster in a host potential.
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> import galax.potential as gp
+    >>> from galax.dynamics.cluster import lagrange_points
+
+    >>> pot = gp.MilkyWayPotential()
+    >>> x = cx.CartesianPos3D.from_(u.Quantity([8.0, 0.0, 0.0], "kpc"))
+    >>> v = cx.CartesianVel3D.from_(u.Quantity([0.0, 220.0, 0.0], "km/s"))
+    >>> mass = u.Quantity(1e4, "Msun")
+    >>> t = u.Quantity(0.0, "Gyr")
+
+    This also works with a `galax.coordinates.PhaseSpacePosition` object:
+
+    >>> import galax.coordinates as gc
+    >>> w = gc.PhaseSpacePosition(q=x, p=v)
+    >>> lpts = lagrange_points(pot, w, mass=mass, time=t)
+    >>> lpts.l1
+    Quantity['length'](Array([7.97070926, 0. , 0. ], dtype=float64), unit='kpc')
+    >>> lpts.l2
+    Quantity['length'](Array([8.02929074, 0. , 0. ], dtype=float64), unit='kpc')
+
+    """
+    return lagrange_points(pot, w.q, w.p, mass=mass, t=time)
 
 
 @dispatch

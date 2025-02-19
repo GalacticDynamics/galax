a5bad594f98d2d6cceca24381ea5165745095ae6
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-02-18 21:25:39 -0500

    ♻️ refactor(pootential): parse_to_xyz_t
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/galax/potential/_src/utils.py
+++ src/galax/potential/_src/utils.py
@@ -4,19 +4,24 @@ __all__: list[str] = []
 
 from typing import Any
 
+import equinox as eqx
 import numpy as np
 from jax.dtypes import canonicalize_dtype
 from jaxtyping import ArrayLike
-from plum import convert, dispatch
+from plum import Dispatcher, convert, dispatch
 
 import coordinax as cx
+import coordinax.frames as cxf
 import quaxed.numpy as jnp
 import unxt as u
-from unxt.quantity import AbstractQuantity
+from unxt.quantity import AbstractQuantity, BareQuantity
 from unxt.unitsystems import AbstractUnitSystem
 
 import galax.coordinates as gc
 import galax.typing as gt
+from galax.utils._unxt import AllowValue
+
+speed_of_light = u.quantity.BareQuantity(299_792_458, "m/s")
 
 
 def parse_dtypes(dtype2: np.dtype, dtype1: Any, /) -> np.dtype | None:
@@ -24,6 +29,477 @@ def parse_dtypes(dtype2: np.dtype, dtype1: Any, /) -> np.dtype | None:
         dtype2
         if dtype1 is None
         else jnp.promote_types(dtype2, canonicalize_dtype(dtype1))
+    )
+
+
+# ==============================================================================
+
+coord_dispatcher = Dispatcher(warn_redefinition=True)
+
+
+@coord_dispatcher.abstract
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None, *args: Any, **kwargs: Any
+) -> tuple[Any, Any]:  # pos, time
+    """Parse input arguments to position & time.
+
+    Examples
+    --------
+    >>> import quaxed.numpy as jnp
+    >>> import unxt as u
+    >>> import coordinax as cx
+    >>> import galax.coordinates as gc
+    >>> from galax.potential._src.utils import parse_to_xyz_t
+
+    - `jax.Array`-like:
+
+    >>> xyz = [1, 0, 0]
+    >>> t = 0
+    >>> parse_to_xyz_t(None, xyz, t, dtype=float)
+    (Array([1., 0., 0.], dtype=float64),
+     Array(0., dtype=float64))
+
+    - `jax.Array`:
+
+    >>> xyz = jnp.array([1, 0, 0])
+    >>> t = jnp.array(0)
+    >>> parse_to_xyz_t(None, xyz, t, dtype=float)
+    (Array([1., 0., 0.], dtype=float64),
+     Array(0., dtype=float64))
+
+    >>> txyz = jnp.array([0, 1, 0, 0])
+    >>> parse_to_xyz_t(None, txyz, dtype=float)
+    (Array([1., 0., 0.], dtype=float64),
+     Array(0., dtype=float64))
+
+    >>> parse_to_xyz_t(None, txyz, t, dtype=float)
+    (Array([1., 0., 0.], dtype=float64),
+     Array(0., dtype=float64))
+
+    - `unxt.Quantity`:
+
+    >>> q = u.Quantity([1, 0, 0], "kpc")
+    >>> t = u.Quantity(1, "Gyr")
+    >>> parse_to_xyz_t(None, q, t)
+    (Quantity['length'](Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, ...), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, q, t, ustrip=u.unitsystems.galactic)
+    (Array([1, 0, 0], dtype=int64),
+     Array(1000., dtype=float64, weak_type=True))
+
+    >>> tq = u.Quantity([0, 1, 0, 0], "kpc")
+    >>> parse_to_xyz_t(None, tq)
+    (Quantity['length'](Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(0., dtype=float64), unit='kpc s / m'))
+
+    >>> parse_to_xyz_t(None, tq, u.Quantity(0, "Gyr"))
+    (Quantity['length'](Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(0., dtype=float64), unit='kpc s / m'))
+
+    - `coordinax.AbstractVector` objects:
+
+    >>> q = cx.vecs.CartesianPos3D.from_([1, 0, 0], "kpc")
+    >>> parse_to_xyz_t(None, q, t)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, q, t, ustrip=u.unitsystems.galactic)
+    (Array([1, 0, 0], dtype=int64),
+     Array(1000., dtype=float64, weak_type=True))
+
+    >>> tq = cx.vecs.FourVector(q=q, t=t)
+    >>> parse_to_xyz_t(None, tq)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, tq, t)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    - `coordinax.Space` objects:
+
+    >>> space = cx.Space(length=q)
+    >>> parse_to_xyz_t(None, space, t)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, space, t, ustrip=u.unitsystems.galactic)
+    (Array([1, 0, 0], dtype=int64),
+     Array(1000., dtype=float64, weak_type=True))
+
+    >>> space = cx.Space(length=tq)
+    >>> parse_to_xyz_t(None, space)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, space, t)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    - `coordinax.AbstractCoordinate` objects:
+
+    >>> coord = cx.Coordinate(cx.Space(length=q), frame=gc.frames.simulation_frame)
+    >>> parse_to_xyz_t(None, coord, t)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, coord, t, ustrip=u.unitsystems.galactic)
+    (Array([1, 0, 0], dtype=int64),
+     Array(1000., dtype=float64, weak_type=True))
+
+    >>> coord = cx.Coordinate(cx.Space(length=tq), frame=gc.frames.simulation_frame)
+    >>> parse_to_xyz_t(None, coord)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, coord, t)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    - `galax.coordinates.PhaseSpacePosition` objects:
+
+    >>> p = cx.vecs.CartesianVel3D.from_([0, 0, 0], "km/s")
+    >>> w = gc.PhaseSpacePosition(q=q, p=p)
+    >>> parse_to_xyz_t(None, w, t)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, w, t, ustrip=u.unitsystems.galactic)
+    (Array([1, 0, 0], dtype=int64),
+     Array(1000., dtype=float64, weak_type=True))
+
+    - `galax.coordinates.PhaseSpaceCoordinate` objects:
+
+    >>> wt = gc.PhaseSpaceCoordinate(q=q, p=p, t=t)
+
+    >>> parse_to_xyz_t(None, wt)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    >>> parse_to_xyz_t(None, wt, ustrip=u.unitsystems.galactic)
+    (Array([1, 0, 0], dtype=int64),
+     Array(1000., dtype=float64, weak_type=True))
+
+    >>> parse_to_xyz_t(None, wt, t)
+    (BareQuantity(Array([1, 0, 0], dtype=int64), unit='kpc'),
+     Quantity['time'](Array(1, dtype=int64, weak_type=True), unit='Gyr'))
+
+    """
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    xyz: gt.XYZArrayLike,
+    t: gt.BBtRealLikeSz0,  # TODO: consider also "*#batch 1"
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: None = None,  # noqa: ARG001
+) -> tuple[gt.BBtRealSz3, gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    # Process the input arguments into arrays
+    xyz = jnp.asarray(xyz, dtype=dtype)
+    t = jnp.asarray(t, dtype=dtype)
+
+    # The coordinates are assumed to be in the simulation frame and may need to
+    # be transformed to the target frame.
+    if to_frame is not None:
+        op = cxf.frame_transform_op(gc.frames.simulation_frame, to_frame)
+        xyz, t = op(xyz, t)
+
+    return xyz, t
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    txyz: gt.BBtRealLikeSz4,  # Cartesian, in the reference frame
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: None = None,
+) -> tuple[gt.BBtRealSz3, gt.BBtRealSz0]:
+    """Parse input argument to position & time."""
+    txyz = jnp.asarray(txyz, dtype=dtype)
+    return parse_to_xyz_t(
+        to_frame, txyz[..., 1:4], txyz[..., 0], dtype=None, ustrip=ustrip
+    )
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    txyz: gt.BBtRealLikeSz4,  # Cartesian, in the reference frame
+    t_ref: gt.BBtRealLikeSz0 | None,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: None = None,
+) -> tuple[gt.BBtRealSz3, gt.BBtRealSz0]:
+    """Parse input argument to position & time."""
+    txyz = jnp.asarray(txyz, dtype=dtype)
+    t, xyz = txyz[..., 0], txyz[..., 1:4]
+    t = eqx.error_if(
+        t,
+        t_ref is not None and jnp.logical_not(jnp.array_equal(t_ref, t)),
+        "t != txyz[..., 0], None",
+    )
+    return parse_to_xyz_t(to_frame, xyz, t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher.multi(
+    (cxf.AbstractReferenceFrame | None, gt.BBtRealQuSz3, gt.BBtRealQuSz0),
+    (cxf.AbstractReferenceFrame | None, gt.BBtRealQuSz3, gt.BBtRealSz0 | float | int),
+)
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    xyz: gt.BBtRealQuSz3 | gt.BBtRealSz3,
+    t: gt.BBtRealQuSz0 | gt.BBtRealSz0 | float | int,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3 | gt.BBtRealSz3, gt.BBtRealQuSz0 | gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    xyz = jnp.asarray(xyz, dtype=dtype)
+    t = jnp.asarray(t, dtype=dtype)
+
+    if ustrip is not None:
+        xyz = u.ustrip(AllowValue, ustrip["length"], xyz)
+        t = u.ustrip(AllowValue, ustrip["time"], t)
+
+    # The coordinates are assumed to be in the simulation frame and may need to
+    # be transformed to the target frame.
+    if to_frame is not None:
+        op = cxf.frame_transform_op(gc.frames.simulation_frame, to_frame)
+        xyz, t = op(xyz, t)
+
+    return xyz, t
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    txyz: gt.BBtRealQuSz4,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3 | gt.BBtRealSz3, gt.BBtRealQuSz0 | gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    ct, xyz = txyz[..., 0], txyz[..., 1:4]
+    t = ct / speed_of_light
+    return parse_to_xyz_t(to_frame, xyz, t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    txyz: gt.BBtRealQuSz4,
+    tref: gt.BBtRealQuSz0 | None,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3 | gt.BBtRealSz3, gt.BBtRealQuSz0 | gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    ct, xyz = txyz[..., 0], txyz[..., 1:4]
+    t = ct / speed_of_light
+    t = eqx.error_if(
+        t,
+        tref is not None and jnp.logical_not(jnp.array_equal(tref, t)),
+        "t != txyz[..., 0], None",
+    )
+    return parse_to_xyz_t(to_frame, xyz, t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    q: cx.vecs.AbstractPos3D,
+    t: Any,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3 | gt.BBtRealSz3, gt.BBtRealQuSz0 | gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    xyz = convert(q.vconvert(cx.CartesianPos3D), BareQuantity)
+    return parse_to_xyz_t(to_frame, xyz, t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    q4: cx.vecs.FourVector,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3, gt.BBtRealQuSz0]:
+    """Parse input arguments to position & time."""
+    return parse_to_xyz_t(to_frame, q4.q, q4.t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    q4: cx.vecs.FourVector,
+    tref: gt.BBtQuSz0 | None,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3, gt.BBtRealQuSz0]:
+    """Parse input arguments to position & time."""
+    t = q4.t
+    t = eqx.error_if(
+        t,
+        tref is not None and jnp.logical_not(jnp.array_equal(tref, t)),
+        "t != q4.t, None",
+    )
+    return parse_to_xyz_t(to_frame, q4.q, t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    space: cx.vecs.Space,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3 | gt.BBtRealSz3, gt.BBtRealQuSz0 | gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    q = space["length"]
+    q = eqx.error_if(q, not isinstance(q, cx.vecs.FourVector), "q is not a FourVector")
+    return parse_to_xyz_t(to_frame, q, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    space: cx.vecs.Space,
+    t: Any,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3 | gt.BBtRealSz3, gt.BBtRealQuSz0 | gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    q = space["length"]
+
+    # Case 1: 3D position requires time
+    if isinstance(q, cx.vecs.AbstractPos3D):
+        t = eqx.error_if(t, t is None, "t is required")
+        return parse_to_xyz_t(to_frame, q, t, dtype=dtype, ustrip=ustrip)
+
+    # Case 2: 4D position, time must be equal or None
+    if isinstance(q, cx.vecs.FourVector):
+        return parse_to_xyz_t(to_frame, q, t, dtype=dtype, ustrip=ustrip)
+
+    msg = f"Unsupported position type: {type(q)}"
+    raise TypeError(msg)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    coord: cxf.AbstractCoordinate,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3 | gt.BBtRealSz3, gt.BBtRealQuSz0 | gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    # Transform to the frame
+    # TODO: think about the transformation of the time
+    coord = coord.to_frame(gc.frames.simulation_frame if to_frame is None else to_frame)
+    # Re-dispatch on the data
+    # Now that the data is in the correct frame, we can just parse the data.
+    return parse_to_xyz_t(None, coord.data, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    coord: cxf.AbstractCoordinate,
+    t: Any,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3 | gt.BBtRealSz3, gt.BBtRealQuSz0 | gt.BBtRealSz0]:
+    """Parse input arguments to position & time."""
+    # Transform to the frame
+    # TODO: think about the transformation of the time
+    coord = coord.to_frame(gc.frames.simulation_frame if to_frame is None else to_frame)
+    # Re-dispatch on the data
+    # Now that the data is in the correct frame, we can just parse the data.
+    return parse_to_xyz_t(None, coord.data, t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    w: gc.PhaseSpacePosition,
+    t: Any,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3, gt.BBtRealQuSz0]:
+    """Parse input arguments to position & time."""
+    # Transform to the frame
+    # TODO: think about the transformation of the time
+    w = w.to_frame(gc.frames.simulation_frame if to_frame is None else to_frame)
+    # Re-dispatch on the data
+    # Now that the data is in the correct frame, we can just parse the data.
+    return parse_to_xyz_t(None, w.q, t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    wt: gc.AbstractPhaseSpaceCoordinate,
+    t: Any,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3, gt.BBtRealQuSz0]:
+    """Parse input arguments to position & time."""
+    # Transform to the frame
+    wt = wt.to_frame(gc.frames.simulation_frame if to_frame is None else to_frame)
+    # Parse `t`
+    t = eqx.error_if(
+        jnp.asarray(wt.t, dtype=dtype),
+        t is not None and jnp.logical_not(jnp.array_equal(wt.t, t)),
+        "t != wt.t, None",
+    )
+    # Re-dispatch on the data
+    # Now that the data is in the correct frame, we can just parse the data.
+    return parse_to_xyz_t(None, wt.q, t, dtype=dtype, ustrip=ustrip)
+
+
+@coord_dispatcher
+def parse_to_xyz_t(
+    to_frame: cxf.AbstractReferenceFrame | None,
+    wt: gc.AbstractPhaseSpaceCoordinate,
+    /,
+    *,
+    dtype: Any = None,
+    ustrip: u.AbstractUnitSystem | None = None,
+) -> tuple[gt.BBtRealQuSz3, gt.BBtRealQuSz0]:
+    """Parse input arguments to position & time."""
+    # Transform to the frame
+    wt = wt.to_frame(gc.frames.simulation_frame if to_frame is None else to_frame)
+    # Re-dispatch on the data
+    # Now that the data is in the correct frame, we can just parse the data.
+    return parse_to_xyz_t(
+        None, wt.q, jnp.asarray(wt.t, dtype=dtype), dtype=dtype, ustrip=ustrip
     )
 
 
@@ -78,7 +554,7 @@ def parse_to_quantity_or_array(space: cx.vecs.Space, /, **kw: Any) -> gt.BtRealQ
 
 @dispatch
 def parse_to_quantity_or_array(
-    coord: cx.frames.AbstractCoordinate, /, **kw: Any
+    coord: cxf.AbstractCoordinate, /, **kw: Any
 ) -> gt.BtRealQuSz3:
     coord = coord.to_frame(gc.frames.simulation_frame)  # TODO: frame
     return parse_to_quantity_or_array(coord.data, **kw)
--- src/galax/typing.py
+++ src/galax/typing.py
@@ -22,7 +22,7 @@ __all__: list[str] = []
 from typing import TypeAlias
 
 import astropy.units as apyu
-from jaxtyping import Array, ArrayLike, Float, Int, Real, Scalar, Shaped
+from jaxtyping import Array, ArrayLike, Float, Int, Real, Scalar, ScalarLike, Shaped
 
 import unxt as u
 from unxt.quantity import AbstractQuantity
@@ -72,6 +72,10 @@ RealSz0: TypeAlias = Real[Array, ""]
 BtRealSz0: TypeAlias = Shaped[RealSz0, "*batch"]
 BBtRealSz0: TypeAlias = Shaped[RealSz0, "*#batch"]
 
+RealLikeSz0: TypeAlias = Real[ArrayLike, ""]
+BtRealLikeSz0: TypeAlias = Shaped[RealLikeSz0, "*batch"]
+BBtRealLikeSz0: TypeAlias = Shaped[RealLikeSz0, "*#batch"]
+
 RealQuSz0: TypeAlias = Real[AbstractQuantity, ""]
 BtRealQuSz0: TypeAlias = Shaped[RealQuSz0, "*batch"]
 BBtRealQuSz0: TypeAlias = Shaped[RealQuSz0, "*#batch"]
@@ -89,7 +93,6 @@ Sz3: TypeAlias = Shaped[Array, "3"]
 BtSz3: TypeAlias = Shaped[Sz3, "*batch"]
 BBtSz3: TypeAlias = Shaped[Sz3, "*#batch"]
 
-
 QuSz3: TypeAlias = Shaped[AbstractQuantity, "3"]
 BtQuSz3: TypeAlias = Shaped[QuSz3, "*batch"]
 BBtQuSz3: TypeAlias = Shaped[QuSz3, "*#batch"]
@@ -101,12 +104,22 @@ BtFloatQuSz3: TypeAlias = Float[QuSz3, "*batch"]
 BBtFloatQuSz3: TypeAlias = Float[QuSz3, "*#batch"]
 
 RealSz3: TypeAlias = Real[Array, "3"]
-BtRealSz3: TypeAlias = Shaped[RealSz3, "*batch"]
-BBtRealSz3: TypeAlias = Shaped[RealSz3, "*#batch"]
+BtRealSz3: TypeAlias = Real[RealSz3, "*batch"]
+BBtRealSz3: TypeAlias = Real[RealSz3, "*#batch"]
+
+RealLikeSz3: TypeAlias = Real[ArrayLike, "3"]
+BtRealLikeSz3: TypeAlias = Real[RealLikeSz3, "*batch"]
+BBtRealLikeSz3: TypeAlias = Real[RealLikeSz3, "*#batch"]
 
 RealQuSz3: TypeAlias = Real[AbstractQuantity, "3"]
 BtRealQuSz3: TypeAlias = Shaped[RealQuSz3, "*batch"]
 BBtRealQuSz3: TypeAlias = Shaped[RealQuSz3, "*#batch"]
+
+# ---------------------------
+# 4-vector
+
+BBtRealLikeSz4: TypeAlias = Real[ArrayLike, "*#batch 4"]
+BBtRealQuSz4: TypeAlias = Real[u.AbstractQuantity, "*#batch 4"]
 
 # ---------------------------
 # 6-vector
@@ -124,7 +137,6 @@ BBtSz7: TypeAlias = Shaped[Sz7, "*#batch"]
 # 3x3 matrix
 
 Sz33: TypeAlias = Shaped[Array, "3 3"]
-BtSz33: TypeAlias = Shaped[Sz33, "*batch"]
 BBtSz33: TypeAlias = Shaped[Sz33, "*#batch"]
 FloatSz33: TypeAlias = Float[Array, "3 3"]
 
@@ -166,3 +178,12 @@ LengthBBtSz3: TypeAlias = Shaped[LengthSz3, "*#batch"]
 SpeedSz3: TypeAlias = Shaped[u.Quantity["speed"], "3"]
 SpeedBtSz3: TypeAlias = Shaped[SpeedSz3, "*batch"]
 SpeedBBtSz3: TypeAlias = Shaped[SpeedSz3, "*#batch"]
+
+
+# ===============================
+# TODO: sort
+
+
+XYZArrayLike: TypeAlias = (
+    BBtRealLikeSz3 | list[ScalarLike] | tuple[ScalarLike, ScalarLike, ScalarLike]
+)

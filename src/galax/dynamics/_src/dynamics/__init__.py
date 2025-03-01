fc17262d10baab6fa8219ac378bcdd3bbc182f53
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-03-01 12:37:17 -0500

    🚚 resource(dynamics): move parsetime module

--- src/galax/dynamics/_src/dynamics/__init__.py
+++ src/galax/dynamics/_src/dynamics/__init__.py
@@ -6,12 +6,9 @@ __all__ = [
     "AbstractDynamicsField",
     "HamiltonianField",
     "NBodyField",
-    # utils
-    "parse_time_specification",
 ]
 
 from .field_base import AbstractDynamicsField
 from .field_hamiltonian import HamiltonianField
 from .field_nbody import NBodyField
-from .parsetime import parse_time_specification
 from .solver import DynamicsSolver
--- src/galax/dynamics/integrate.py
+++ src/galax/dynamics/integrate.py
@@ -13,13 +13,13 @@ from jaxtyping import install_import_hook
 from galax.setup_package import RUNTIME_TYPECHECKER
 
 with install_import_hook("galax.dynamics.integrate", RUNTIME_TYPECHECKER):
-    from ._src.dynamics import parse_time_specification
     from ._src.legacy import (
         Integrator,
         InterpolatedPhaseSpaceCoordinate,
         evaluate_orbit,
     )
     from ._src.orbit import PhaseSpaceInterpolation
+    from ._src.parsetime import parse_time_specification
 
 # Cleanup
 del install_import_hook, RUNTIME_TYPECHECKER

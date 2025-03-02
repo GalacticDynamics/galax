a13fd71aae02b97bee212402a67d37b7672169fd
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-03-02 16:03:45 -0500

    🎨 style(potential): param import structure (#702)
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/galax/potential/_src/params/__init__.py
+++ src/galax/potential/_src/params/__init__.py
@@ -1,3 +1,26 @@
-""":mod:`galax.potential.params`."""
+"""`galax.potential.params`."""
 
-__all__: list[str] = []
+__all__ = [
+    # Fields
+    "ParameterField",
+    # Parameters
+    "AbstractParameter",
+    "ParameterCallable",
+    "ConstantParameter",
+    "LinearParameter",
+    "CustomParameter",
+    # Attributes
+    "AbstractParametersAttribute",
+    "ParametersAttribute",
+    "CompositeParametersAttribute",
+]
+
+from .attr import (
+    AbstractParametersAttribute,
+    CompositeParametersAttribute,
+    ParametersAttribute,
+)
+from .base import AbstractParameter, ParameterCallable
+from .constant import ConstantParameter
+from .core import CustomParameter, LinearParameter
+from .field import ParameterField

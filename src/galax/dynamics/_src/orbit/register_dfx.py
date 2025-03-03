ea24056e6f56217ad7601b196f8918ffc5e06ac7
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-03-03 13:31:15 -0500

    ♻️ refactor(dynamics): consolidate Solution constructors (#712)
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/galax/dynamics/_src/orbit/register_dfx.py
+++ src/galax/dynamics/_src/orbit/register_dfx.py
@@ -10,14 +10,94 @@ __all__: list[str] = []
 import diffrax as dfx
 
 import coordinax as cx
+import quaxed.numpy as jnp
 import unxt as u
+from unxt.quantity import BareQuantity as FastQ
 
 import galax.coordinates as gc
 from .interp import PhaseSpaceInterpolation
 from .orbit import Orbit
 
 
-@gc.AbstractPhaseSpaceObject.from_.dispatch  # type: ignore[misc,attr-defined]
+@gc.PhaseSpaceCoordinate.from_.dispatch
+def from_(
+    cls: type[gc.PhaseSpaceCoordinate],
+    soln: dfx.Solution,
+    /,
+    *,
+    frame: cx.frames.AbstractReferenceFrame,  # not dispatched on, but required
+    units: u.AbstractUnitSystem,  # not dispatched on, but required
+    unbatch_time: bool = True,
+) -> gc.PhaseSpaceCoordinate:
+    r"""Convert a solution to a phase-space position.
+
+    This assumes that the solution is shaped as:
+    - t0, t1: (*tbatch,)
+    - ts: (*tbatch, [T])
+    - *ys: (*tbatch, [T], *ybatch, 3)
+
+    Examples
+    --------
+    >>> import unxt as u
+    >>> import galax.coordinates as gc
+    >>> import galax.potential as gp
+    >>> import galax.dynamics as gd
+
+    >>> pot = gp.HernquistPotential(m_tot=1e12, r_s=5, units="galactic")
+    >>> field = gd.fields.HamiltonianField(pot)
+    >>> solver = gd.OrbitSolver()  # defaults to Dopri8
+    >>> w0 = gc.PhaseSpaceCoordinate(
+    ...     q=u.Quantity([[8, 0, 9], [9, 0, 3]], "kpc"),
+    ...     p=u.Quantity([0, 220, 0], "km/s"),
+    ...     t=u.Quantity(0, "Gyr"))
+    >>> t1 = u.Quantity(1, "Gyr")
+    >>> soln = solver.solve(field, w0, t1)
+    >>> soln.ts.shape, soln.ys[0].shape
+    ((1,), (1, 2, 3))
+
+    >>> w = gc.PhaseSpaceCoordinate.from_(soln, units=pot.units, frame=w0.frame)
+    >>> print(w, w.shape, sep='\n')
+    PhaseSpaceCoordinate(
+        q=<CartesianPos3D (x[kpc], y[kpc], z[kpc])
+            [[-5.151 -6.454 -5.795]
+             [ 4.277  4.633  1.426]]>,
+        p=<CartesianVel3D (x[kpc / Myr], y[kpc / Myr], z[kpc / Myr])
+            [[ 0.225 -0.068  0.253]
+             [-0.439 -0.002 -0.146]]>,
+        t=Quantity['time'](Array(1000., dtype=float64), unit='Myr'),
+        frame=SimulationFrame())
+    (2,)
+
+    >>> w = gc.PhaseSpaceCoordinate.from_(soln, units=pot.units, frame=w0.frame,
+    ...                                   unbatch_time=False)
+    >>> print(w.shape)  # (*batch, [time])
+    (2, 1)
+
+    """
+    # Reshape (*tbatch, T, *ybatch, *shape) to (*tbatch, *ybatch, T, *shape)
+    t = soln.ts  # already in the shape (*tbatch, T)
+    n_tbatch = soln.t0.ndim
+    q, p = soln.ys
+    if n_tbatch < q.ndim - 1:
+        q = jnp.moveaxis(q, n_tbatch, -2)
+        p = jnp.moveaxis(p, n_tbatch, -2)
+
+    # Reshape (*tbatch, *ybatch, T) to (*tbatch, *ybatch) if T == 1
+    if unbatch_time and t.ndim > 0 and t.shape[-1] == 1:
+        t = t[..., -1]
+        q = q[..., -1, :]
+        p = p[..., -1, :]
+
+    # Convert the solution to a phase-space position
+    return cls(
+        q=cx.CartesianPos3D.from_(q, units["length"]),
+        p=cx.CartesianVel3D.from_(p, units["speed"]),
+        t=FastQ(t, units["time"]),
+        frame=frame,
+    )
+
+
+@gc.AbstractPhaseSpaceObject.from_.dispatch
 def from_(
     cls: type[Orbit],
     soln: dfx.Solution,

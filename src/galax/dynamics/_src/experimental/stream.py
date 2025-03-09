61e9598755634967132ef8bab572a5fb9bc8c390
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-03-09 18:35:01 +0100

    ✨ feat(dynamics): StreamSimulator
    Signed-off-by: Nathaniel Starkman <nstarman@users.noreply.github.com>

--- src/galax/dynamics/_src/experimental/stream.py
+++ src/galax/dynamics/_src/experimental/stream.py
@@ -3,17 +3,19 @@
 __all__: list[str] = []
 
 from functools import partial
-from typing import Any, TypeAlias, cast
+from typing import Any, TypeAlias, cast, final
 from typing_extensions import Unpack
 
 import diffrax as dfx
 import jax
 import jax.random as jr
+from jax.tree_util import register_dataclass
 from jaxtyping import Array, PRNGKeyArray, Real
 from plum import dispatch
 
 import diffraxtra as dfxtra
 import quaxed.numpy as jnp
+from dataclassish.converters import dataclass
 
 import galax._custom_types as gt
 import galax.dynamics._src.custom_types as gdt
@@ -36,98 +38,110 @@ default_solver = dfxtra.DiffEqSolver(
 
 default_kinematic_df = Fardal2015DF()
 
+
 ##############################################################################
+
 
 ICSScanIn: TypeAlias = tuple[gt.Sz0, gdt.Qarr, gdt.Parr, gt.Sz0]  # t, x, v, Msat
 ICSScanOut: TypeAlias = tuple[gdt.Qarr, gdt.Parr, gdt.Qarr, gdt.Parr]  # x/v_l1, x/v_l2
 ICSScanCarry: TypeAlias = tuple[PRNGKeyArray, Unpack[ICSScanOut]]
 
 
-# TODO: fold this into a SprayDF
-@partial(jax.jit, static_argnames=("solver", "solver_kwargs"))
-def generate_stream_ics(
-    pot: gp.AbstractPotential,
-    release_times: gt.SzTime,
-    prog_w0: gdt.QParr,
-    /,
-    Msat: gt.LikeSz0 | gt.SzTime,  # can be time-dependent by matching release_times
-    kinematic_df: AbstractKinematicDF | None = None,
-    *,
-    key: PRNGKeyArray,
-    solver: dfxtra.AbstractDiffEqSolver = default_solver,
-    solver_kwargs: dict[str, Any] | None = None,
-) -> tuple[SzN3, SzN3, SzN3, SzN3]:
-    """Generate the initial conditions for the stream particles.
+# TODO: rename to Fardal-like, since it can have different parameters?
+@final
+@partial(
+    register_dataclass,
+    data_fields=[],
+    meta_fields=[],
+)
+@dataclass
+class StreamSimulator:
+    @partial(jax.jit, static_argnames=("solver", "solver_kwargs"))
+    def init(
+        self,
+        pot: gp.AbstractPotential,
+        release_times: gt.SzTime,
+        prog_w0: gdt.QParr,
+        /,
+        Msat: gt.LikeSz0 | gt.SzTime,  # can be time-dependent by matching release_times
+        kinematic_df: AbstractKinematicDF | None = None,
+        *,
+        key: PRNGKeyArray,
+        solver: dfxtra.AbstractDiffEqSolver = default_solver,
+        solver_kwargs: dict[str, Any] | None = None,
+    ) -> tuple[SzN3, SzN3, SzN3, SzN3]:
+        """Generate the initial conditions for the stream particles.
 
-    This function generates the initial conditions for the stream particles
-    given the progenitor's orbit, the release times, and the progenitor's
-    mass.
+        This function generates the initial conditions for the stream particles
+        given the progenitor's orbit, the release times, and the progenitor's
+        mass.
 
-    """
-    # Integrate the progenitor's orbit to get the stream progenitor's positions
-    # and velocities at the stream particle release times.
-    prog_xs, prog_vs = integrate_orbit(
-        pot,
-        prog_w0,
-        saveat=release_times,
-        solver=solver,
-        solver_kwargs=solver_kwargs,
-    ).ys
+        """
+        # Integrate the progenitor's orbit to get the stream progenitor's positions
+        # and velocities at the stream particle release times.
+        prog_xs, prog_vs = integrate_orbit(
+            pot,
+            prog_w0,
+            saveat=release_times,
+            solver=solver,
+            solver_kwargs=solver_kwargs,
+        ).ys
 
-    w0_df = default_kinematic_df if kinematic_df is None else kinematic_df
+        w0_df = default_kinematic_df if kinematic_df is None else kinematic_df
 
-    # Define the scan function for generating the stream particle's initial
-    # conditions given the release time, position, velocity, and Msat.
-    def scan_fn(carry: ICSScanCarry, x: ICSScanIn) -> tuple[ICSScanCarry, ICSScanOut]:
-        key, subkey = jr.split(carry[0])
-        xv_l12_new = w0_df.sample(subkey, pot, t=x[0], x=x[1], v=x[2], Msat=x[3])
-        new_carry = (key, *xv_l12_new)
-        return new_carry, xv_l12_new
+        # Define the scan function for generating the stream particle's initial
+        # conditions given the release time, position, velocity, and Msat.
+        def scan_fn(
+            carry: ICSScanCarry, x: ICSScanIn
+        ) -> tuple[ICSScanCarry, ICSScanOut]:
+            key, subkey = jr.split(carry[0])
+            xv_l12_new = w0_df.sample(subkey, pot, t=x[0], x=x[1], v=x[2], Msat=x[3])
+            new_carry = (key, *xv_l12_new)
+            return new_carry, xv_l12_new
 
-    # Initial carry is [key, x_l1, v_l1, x_l2, v_l2]
-    init_carry = (key, jnp.zeros(3), jnp.zeros(3), jnp.zeros(3), jnp.zeros(3))
-    Msat = Msat * jnp.ones(len(release_times))  # shape match for scanning
+        # Initial carry is [key, x_l1, v_l1, x_l2, v_l2]
+        init_carry = (key, jnp.zeros(3), jnp.zeros(3), jnp.zeros(3), jnp.zeros(3))
+        Msat = Msat * jnp.ones(len(release_times))  # shape match for scanning
 
-    # Scan over the release times/xs/vs/ms to generate the stream particle's
-    # initial conditions.
-    _, all_states = jax.lax.scan(
-        scan_fn, init_carry, (release_times, prog_xs, prog_vs, Msat)
-    )
-    return cast(ICSScanOut, all_states)
+        # Scan over the release times/xs/vs/ms to generate the stream particle's
+        # initial conditions.
+        _, all_states = jax.lax.scan(
+            scan_fn, init_carry, (release_times, prog_xs, prog_vs, Msat)
+        )
+        return cast(ICSScanOut, all_states)
 
+    @dispatch.abstract
+    def run(self, *args: Any, **kwargs: Any) -> Any:
+        """Simulate a stellar stream.
 
-##############################################################################
+        Examples
+        --------
+        >>> import jax.numpy as jnp
+        >>> import jax.random as jr
+        >>> import galax.potential as gp
+        >>> import galax.dynamics as gd
 
+        >>> pot = gp.HernquistPotential(1e12, 10, units="galactic")
+        >>> qp = (jnp.array([15.0, 0.0, 0.0]), jnp.array([0.0, 0.225, 0.0]))
+        >>> ts = jnp.linspace(-4_000, -150, 2_000)
+        >>> t1 = 0.0
+        >>> Msat = 1e5
 
-@dispatch.abstract
-def simulate_stream(*args: Any, **kwargs: Any) -> Any:
-    """Simulate a stellar stream.
+        >>> stream_simulator = gd.experimental.stream.StreamSimulator()
 
-    Examples
-    --------
-    >>> import jax.numpy as jnp
-    >>> import jax.random as jr
-    >>> import galax.potential as gp
-    >>> import galax.dynamics as gd
+        >>> stream_lead, stream_trail = stream_simulator.run(
+        ...     pot, qp, release_times=ts, t1=t1, Msat=Msat, key=jr.key(0))
 
-    >>> pot = gp.HernquistPotential(1e12, 10, units="galactic")
-    >>> qp = (jnp.array([15.0, 0.0, 0.0]), jnp.array([0.0, 0.225, 0.0]))
-    >>> ts = jnp.linspace(-4_000, -150, 2_000)
-    >>> t1 = 0.0
-    >>> Msat = 1e5
-
-    >>> stream_lead, stream_trail = gd.experimental.stream.simulate_stream(
-    ...     pot, qp, release_times=ts, t1=t1, Msat=Msat, key=jr.key(0))
-
-    """
+        """
 
 
 # ---------------------------
 # auto-determine
 
 
-@dispatch
-def simulate_stream(
+@StreamSimulator.run.dispatch
+def run(
+    self: StreamSimulator,
     pot: gp.AbstractPotential,
     prog_w0: gdt.QParr,
     /,
@@ -141,7 +155,7 @@ def simulate_stream(
     solver: dfxtra.AbstractDiffEqSolver = default_solver,
     solver_kwargs: dict[str, Any] | None = None,
 ) -> Any:
-    return simulate_stream(
+    return self.run(
         lstrat.Determine,
         pot,
         prog_w0,
@@ -156,8 +170,9 @@ def simulate_stream(
     )
 
 
-@dispatch
-def simulate_stream(
+@StreamSimulator.run.dispatch
+def run(
+    self: StreamSimulator,
     loop_strategy: type[lstrat.Determine],  # noqa: ARG001
     pot: gp.AbstractPotential,
     prog_w0: gdt.QParr,
@@ -174,7 +189,7 @@ def simulate_stream(
 ) -> Any:
     # Determine the loop strategy
     loop_strat = lstrat.VMap  # TODO: an actual heuristic
-    return simulate_stream(
+    return self.run(
         loop_strat,
         pot,
         prog_w0,
@@ -196,13 +211,14 @@ StreamScanOut: TypeAlias = tuple[gdt.Qarr, gdt.Parr, gdt.Qarr, gdt.Parr]
 StreamCarry: TypeAlias = tuple[int, Unpack[StreamScanOut]]
 
 
-@dispatch
+@StreamSimulator.run.dispatch
 @partial(
     jax.jit,
-    static_argnums=(0,),
+    static_argnums=(1,),
     static_argnames=("dense", "solver", "solver_kwargs"),
 )
-def simulate_stream(
+def run(
+    self: StreamSimulator,
     loop_strategy: type[lstrat.Scan],  # noqa: ARG001
     pot: gp.AbstractPotential,
     prog_w0: gdt.QParr,
@@ -223,7 +239,7 @@ def simulate_stream(
 
     """
     t1 = jnp.asarray(t1)
-    x0s_l1, v0s_l1, x0s_l2, v0s_l2 = generate_stream_ics(  # x/v_l1/2 shape (N, 3)
+    x0s_l1, v0s_l1, x0s_l2, v0s_l2 = self.init(  # x/v_l1/2 shape (N, 3)
         pot,
         jnp.concatenate([release_times, t1[None]]),
         prog_w0,
@@ -273,13 +289,14 @@ def simulate_stream(
     return out
 
 
-@dispatch
+@StreamSimulator.run.dispatch
 @partial(
     jax.jit,
-    static_argnums=(0,),
+    static_argnums=(1,),
     static_argnames=("dense", "solver", "solver_kwargs"),
 )
-def simulate_stream(
+def run(
+    self: StreamSimulator,
     loop_strategy: type[lstrat.VMap],  # noqa: ARG001
     pot: gp.AbstractPotential,
     prog_w0: gdt.QParr,
@@ -295,7 +312,7 @@ def simulate_stream(
     solver_kwargs: dict[str, Any] | None = None,
 ) -> tuple[tuple[SzN3, SzN3], tuple[SzN3, SzN3]] | dfx.Solution:
     t1 = jnp.asarray(t1)
-    x0s_l1, v0s_l1, x0s_l2, v0s_l2 = generate_stream_ics(  # x/v_l1/2 shape (N, 3)
+    x0s_l1, v0s_l1, x0s_l2, v0s_l2 = self.init(  # x/v_l1/2 shape (N, 3)
         pot,
         jnp.concatenate([release_times, t1[None]]),
         prog_w0,

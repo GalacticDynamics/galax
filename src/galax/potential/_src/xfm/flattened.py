e36d821a19a261995ccbc2385c35eaf4ed341563
Adrian Price-Whelan <583379+adrn@users.noreply.github.com>
2025-05-09 08:03:15 -0400

    change axis_ratio to q_z

--- src/galax/potential/_src/xfm/flattened.py
+++ src/galax/potential/_src/xfm/flattened.py
@@ -24,7 +24,7 @@ class FlattenedInThePotential(AbstractTransformedPotential):
     the potential at the transformed coordinates. This potential flattens the z-axis of
     the potential.
 
-    - `axis_ratio`: the scaling of the z-axis
+    - `q_z`: the scaling of the z-axis
 
     This is applied as
 
@@ -32,7 +32,7 @@ class FlattenedInThePotential(AbstractTransformedPotential):
         \Phi'(x, y, z) = \Phi(x, y, z / a)
     $$
 
-    So a `axis_ratio` of 2 means that the z-axis is twice as long as the x- or y-axis.
+    So a `q_z` of 2 means that the z-axis is twice as long as the x- or y-axis.
 
     See also `galax.potential.TriaxialInThePotential` for a triaxial equivalent of this
     wrapper class.
@@ -44,7 +44,7 @@ class FlattenedInThePotential(AbstractTransformedPotential):
     >>> import galax.potential as gp
 
     >>> opot = gp.HernquistPotential(1e12, 10.0, units="galactic")
-    >>> xpot = gp.FlattenedInThePotential(opot, axis_ratio=2.0)
+    >>> xpot = gp.FlattenedInThePotential(opot, q_z=2.0)
 
     >>> x1 = u.Quantity([1, 0, 0], "kpc")
     >>> x2 = u.Quantity([0, 0, 1], "kpc")
@@ -67,10 +67,10 @@ class FlattenedInThePotential(AbstractTransformedPotential):
     #: the original potential
     base_potential: AbstractPotential
 
-    axis_ratio: AbstractParameter = ParameterField(  # type: ignore[assignment]
+    q_z: AbstractParameter = ParameterField(  # type: ignore[assignment]
         default=u.Quantity(1, ""),
         dimensions="dimensionless",
-        doc="ratio of the z-axis to the x- or y-axis",
+        doc="ratio of the z-axis to the x- or y-axis (or cylindrical radius)",
     )
 
     @partial(jax.jit)
@@ -78,7 +78,7 @@ class FlattenedInThePotential(AbstractTransformedPotential):
         xyz = u.ustrip(AllowValue, self.units["length"], xyz.astype(float))
         # Transform the position
         u1 = self.units["dimensionless"]
-        xyz = xyz.at[..., 2].divide(self.axis_ratio(t, ustrip=u1))
+        xyz = xyz.at[..., 2].divide(self.q_z(t, ustrip=u1))
 
         # Evaluate the potential energy at the transformed position, time.
         return self.base_potential._potential(xyz, t)  # noqa: SLF001

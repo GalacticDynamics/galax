🐛 fix(potential): PowerLawCutoffPotential normalization + Multipole check-init (#782)
* 🐛 fix(potential): PowerLawCutoffPotential normalization. See https://github.com/adrn/gala/pull/425
* 🐛 fix(potential): Multipole check-init
* ⬆️ dep-bump: gala v1.10+
* 📝 docs(potential): improve laplacian docs

Signed-off-by: nstarman <nstarman@users.noreply.github.com>

src/galax/potential/_src/builtin/multipole.py
@@ -209,13 +209,11 @@ class MultipolePotential(AbstractMultipolePotential):
     def __check_init__(self) -> None:
         shape = (self.l_max + 1, self.l_max + 1)
         t = u.Quantity(0.0, "Gyr")
-        is_shape, it_shape = self.ISlm(t).shape, self.ITlm(t).shape
-        os_shape, ot_shape = self.OSlm(t).shape, self.OTlm(t).shape
-        # TODO: check shape across time.
+        iss, its = self.ISlm(t).shape, self.ITlm(t).shape
+        oss, ots = self.OSlm(t).shape, self.OTlm(t).shape
+        # Check shapes match expected
         msg = "I/OSlm and I/OTlm must have the shape (l_max + 1, l_max + 1)."
-        pred = jnp.any(
-            jnp.array([x != shape for x in (is_shape, it_shape, os_shape, ot_shape)])
-        )
+        pred = (iss != shape) or (its != shape) or (oss != shape) or (ots != shape)
         _ = eqx.error_if(t, pred, msg)
 
     @ft.partial(jax.jit)

tests/unit/potential/builtin/test_powerlawcutoff.py
@@ -97,7 +97,7 @@ class TestPowerLawCutoffPotential(
     # ==========================================================================
 
     def test_potential(self, pot: gp.PowerLawCutoffPotential, x: gt.QuSz3) -> None:
-        expect = u.Quantity(6.26573365, unit="kpc2 / Myr2")
+        expect = u.Quantity(-1.20227523, unit="kpc2 / Myr2")
         assert jnp.isclose(
             pot.potential(x, t=0), expect, atol=u.Quantity(1e-8, expect.unit)
         )